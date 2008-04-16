6fb09d5b6c7c7d7150e781dc199bbdddd243481d
Heikki Levanto <heikki@indexdata.dk>
2008-04-16 15:36:46 +0200

    More infrastructure for the scheduler. Updating job list seems to work (at least passes a simple test)

---
+++ src/java/com/indexdata/localindexes/scheduler/JobInstance.java
@@ -0,0 +1,43 @@
+
+package com.indexdata.localindexes.scheduler;
+
+import com.indexdata.localindexes.web.entitybeans.Harvestable;
+
+
+
+/**
+ * a JobInstance is one instance of a harvesting job.
+ * It owns the actual harvesting thread, and has some status info,
+ * as well as the parameters required to create the harvesting object.
+ * 
+ * @author heikki
+ */
+public class JobInstance {
+
+    // private Harvester harvesterThing; // from Marc
+    public Harvestable harvestableData; // from Jakub
+    private Thread harvestingThread;
+    public boolean seen; // for checking what has been deleted
+    
+    public JobInstance ( Harvestable hable ) {
+        this.harvestableData = hable;
+        this.seen=false;
+        this.harvestingThread=null;
+    } // JobInstance constructor
+    
+    /**
+     * kill the job instance
+     *   Tell the thread to stop, if running
+     *   Tell the harvesterThing to clean up
+     */
+    public void kill () {
+        // hrvesterThing.kill(); 
+    } //
+    
+    public void startThread() {
+        if ( harvestingThread == null ) {
+        }
+        
+    } // startThread
+    
+} // class JobInstance
