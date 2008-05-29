e43dc4042bed7e1406a694cb29f1fc5c8a938b31
Heikki Levanto <heikki@indexdata.dk>
2008-05-29 12:08:56 +0200

    Re-established the old filestorage (and changed the file name a bit)

---
+++ src/java/com/indexdata/masterkey/localindices/harvest/oai/FileStorage.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 1995-2008, Index Data
+ * All rights reserved.
+ * See the file LICENSE for details.
+ */
+
+package com.indexdata.masterkey.harvest.oai;
+
+import com.indexdata.localindexes.web.entity.Harvestable;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ *
+ * @author jakub
+ */
+public class FileStorage implements HarvestStorage {
+    private String outFileName;
+    private OutputStream fos;
+    
+    public FileStorage(Harvestable harvestable) {
+        this(harvestable.getName() + harvestable.getId());
+    }
+    
+    public FileStorage(String outFileName) {
+        this.outFileName = outFileName;
+    }
+    
+    public void openOutput() throws IOException {
+        fos = new FileOutputStream(outFileName, true);
+    }
+    
+    public void closeOutput() throws IOException {
+        fos.close();
+    }
+
+    public OutputStream getOutputStream() {
+        return fos;
+    }
+    
+    public String getOutFileName() {
+        return outFileName;
+    }
+
+}
