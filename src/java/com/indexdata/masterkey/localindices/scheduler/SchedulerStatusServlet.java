90c6ac010ea2c618c476bbde967605bf021cf2d6
Jakub Skoczen <jakub@indexdata.dk>
2008-06-02 16:00:10 +0200

    More elaborate status messages.

--- src/java/com/indexdata/masterkey/localindices/scheduler/SchedulerStatusServlet.java
+++ src/java/com/indexdata/masterkey/localindices/scheduler/SchedulerStatusServlet.java
@@ -7,6 +7,7 @@
 package com.indexdata.masterkey.localindices.scheduler;
 
 import com.indexdata.masterkey.localindices.entity.Harvestable;
+import com.indexdata.masterkey.localindices.entity.OaiPmhResource;
 import java.io.*;
 import java.net.*;
 
@@ -42,6 +43,16 @@ public class SchedulerStatusServlet extends HttpServlet {
             out.println("<reportedStatus>" + hable.getCurrentStatus() + "</reportedStatus>");
             out.println("<latestStatus>" + ji.getStatus() + "</latestStatus>");
             out.println("<error>" + ji.getError() + "</error>");
+            if(hable instanceof OaiPmhResource) {
+                OaiPmhResource oaiHable = (OaiPmhResource) hable;
+                out.println("<typeSpecific type=\"OaiPmhResource\">");
+                out.println("<url>" + oaiHable.getUrl() + "</url>");
+                out.println("<metadataPrefix>" + oaiHable.getMetadataPrefix() + "</metadataPrefix>");
+                out.println("<setName>" + oaiHable.getOaiSetName() + "</setName>");
+                out.println("<fromDate>" + oaiHable.getFromDate() + "</fromDate>");
+                out.println("<untilDate>" + oaiHable.getUntilDate() + "</untilDate>");
+                out.println("</typeSpecific>");
+            }                
             out.println("</job>");
         }
         out.println("</jobs>");
