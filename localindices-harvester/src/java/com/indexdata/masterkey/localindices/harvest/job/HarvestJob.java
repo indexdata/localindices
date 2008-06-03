54eec704718aeb2dff1a756019b18eae9c4bc224
Jakub Skoczen <jakub@indexdata.dk>
2008-06-03 12:49:44 +0200

    Fixed comments.

--- localindices-harvester/src/java/com/indexdata/masterkey/localindices/harvest/job/HarvestJob.java
+++ localindices-harvester/src/java/com/indexdata/masterkey/localindices/harvest/job/HarvestJob.java
@@ -8,28 +8,47 @@ package com.indexdata.masterkey.localindices.harvest.job;
 
 import com.indexdata.masterkey.localindices.harvest.storage.HarvestStorage;
 
-
 /**
-   Copyright 2008 Index Data ApS
-   http://www.indexdata.com
-   Licensed under the GNU Public License, Version 2.0.
-*/
-
+ * This interface a runnable harvest job controlled by the JobInstance
+ * 
+ * @author jakub
+ */
 public interface HarvestJob extends Runnable {
     
-    /** Stop the current job
-     * Rolls back the current harvest, deleting those files received so far
-     * Leaves the older harvests in place
-     * 
+    /** 
+     * Stop the current job and rollback the current harvest, 
+     * deleting files received so far. Does not touch the older harvests.
      */
     public void kill();
-        
+
+    /**
+     * Get latest harvest status.
+     * 
+     * @return current status
+     */
     public HarvestStatus getStatus();
     
+    /**
+     * Sets the storage for the the harvested data.
+     * @param storage for the harvest
+     */
     public void setStorage(HarvestStorage storage);
+    
+    /**
+     * Returns storage currently used for harvested data.
+     * @return current storage
+     */
     public HarvestStorage getStorage();
     
+    /**
+     * Inform the harvestesting job the the files harvest were received.
+     */
     public void finishReceived();
+    
+    /**
+     * Get last harvesting error.
+     * @return
+     */
     public String getError();
     
 }
