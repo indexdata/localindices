f431bf65b44be7b2dfb59ebbf73d455a9eeb871f
Jakub Skoczen <jakub@bigos.gateway.2wire.net>
2008-09-18 19:45:42 +0200

    Added members that correspond to pazpar2 settings.

--- localindices-dao/src/com/indexdata/masterkey/localindices/web/service/converter/SearchableTypeLayer.java
+++ localindices-dao/src/com/indexdata/masterkey/localindices/web/service/converter/SearchableTypeLayer.java
@@ -21,7 +21,14 @@ public class SearchableTypeLayer extends Layer {
     private String transform;
     private String elementSet;
     private String requestSyntax;
+    private String queryEncoding;
     private String recordEncoding;
+    private String cclMapAu;
+    private String cclMapTi;
+    private String cclMapSu;
+    private String cclMapDate;
+    private String cclMapTerm;
+    private String authentication;
 
     public String getRecordEncoding() {
         return recordEncoding;
@@ -71,5 +78,61 @@ public class SearchableTypeLayer extends Layer {
     public void setZurl(String zurl) {
         this.zurl = zurl;
     }
+    @XmlElement(name="cclmap_au")
+    public String getCclMapAu() {
+        return cclMapAu;
+    }
+
+    public void setCclMapAu(String cclMapAu) {
+        this.cclMapAu = cclMapAu;
+    }
+    @XmlElement(name="cclmap_date")
+    public String getCclMapDate() {
+        return cclMapDate;
+    }
+
+    public void setCclMapDate(String cclMapDate) {
+        this.cclMapDate = cclMapDate;
+    }
+    @XmlElement(name="cclmap_su")
+    public String getCclMapSu() {
+        return cclMapSu;
+    }
+
+    public void setCclMapSu(String cclMapSu) {
+        this.cclMapSu = cclMapSu;
+    }
+    @XmlElement(name="cclmap_term")
+    public String getCclMapTerm() {
+        return cclMapTerm;
+    }
+
+    public void setCclMapTerm(String cclMapTerm) {
+        this.cclMapTerm = cclMapTerm;
+    }
+    @XmlElement(name="cclmap_ti")
+    public String getCclMapTi() {
+        return cclMapTi;
+    }
+
+    public void setCclMapTi(String cclMapTi) {
+        this.cclMapTi = cclMapTi;
+    }
+
+    public String getQueryEncoding() {
+        return queryEncoding;
+    }
+
+    public void setQueryEncoding(String queryEncoding) {
+        this.queryEncoding = queryEncoding;
+    }
+
+    public String getAuthentication() {
+        return authentication;
+    }
+
+    public void setAuthentication(String authentication) {
+        this.authentication = authentication;
+    }       
 
 }
