dca0bd03119c496d776e6acf3da305cddaebb5d8
Heikki Levanto <heikki@indexdata.dk>
2008-10-14 17:42:03 +0200

    Recurses through the websites all right, collects links for the next round Does not yet index the data. Needs more filtering, robots.txt, and much more.

--- localindices-harvester/src/java/com/indexdata/masterkey/localindices/harvest/job/WebHarvestJob.java
+++ localindices-harvester/src/java/com/indexdata/masterkey/localindices/harvest/job/WebHarvestJob.java
@@ -20,13 +20,11 @@ import org.apache.log4j.Logger;
  * @author heikki
  * 
  * TODO:
- *   - Extract links from pages (absolute and relative!)
- *   - Keep track of depth, stop at limit
+ *   - filter what types of links to follow
  *   - Robots.txt (with cache)
  *   - Redirects (watch out for loops etc)
- *   - Do not load a server too hard (if same server as last time, wait a second)
+ *   - better load reduction for servers.
  *   - Get text into zebra
- *   - filter what types of links to follow
  *   - error handling, time outs, etc
  */
 public class WebHarvestJob implements HarvestJob {
@@ -37,19 +35,23 @@ public class WebHarvestJob implements HarvestJob {
     private String error;
     private WebCrawlResource resource;
     private boolean die = false;
-    private Vector<String> toSearch;
-    private Vector<String> searched;
+    private Vector<String> toSearch; // todo list for this round
+    private Vector<String> searched; // all pages we have seen
+    private Vector<String> nextRound; // links found in this round are pushed here
+    private int numtosearch; 
+    private int round;
 
     private class pageInfo {
+
         public String url;
         public String content; // the whole thing
         public String headers;
         public String body;
-        public String[] links;
+        public Vector<String> links;
         public String plaintext;
         public String title;
     }
-    
+
     public WebHarvestJob(WebCrawlResource resource) {
         this.resource = resource;
         this.status = HarvestStatus.NEW;
@@ -58,7 +60,7 @@ public class WebHarvestJob implements HarvestJob {
 
     private synchronized boolean isKillSendt() {
         if (die) {
-            logger.log(Level.WARN, "Bulk harvest received kill signal.");
+            logger.log(Level.WARN, "Web harvest received kill signal.");
         }
         return die;
     }
@@ -99,17 +101,6 @@ public class WebHarvestJob implements HarvestJob {
 
     public String getError() {
         return error;
-    }
-
-    private void initWorkList() {
-        searched = new Vector<String>();
-        toSearch = new Vector<String>();
-        for (String startUrl : resource.getStartUrls().split(" ")) {
-            toSearch.add(startUrl);
-            logger.log(Level.TRACE, "Added start URL '" + startUrl + "'");
-        // Fixme - validate they all are OK
-        }
-
     }
 
     private String fetchPage(String pageUrl) {
@@ -156,48 +147,49 @@ public class WebHarvestJob implements HarvestJob {
                 numRead = urlStream.read(b);
                 if (numRead != -1) {
                     String newContent = new String(b, 0, numRead);
-                    content += newContent;                   
+                    content += newContent;
                 }
             }
             urlStream.close();
-            logger.log(Level.DEBUG, pageUrl + " Read " + content.length() + " bytes" );
+            logger.log(Level.TRACE, pageUrl + " Read " + content.length() + " bytes");
             //logger.log(Level.DEBUG, content );
             return content;
         } catch (IOException ex) {
-            logger.log(Level.ERROR, "I/O Exception with " + pageUrl + ": " + 
+            logger.log(Level.ERROR, "I/O Exception with " + pageUrl + ": " +
                     ex.getMessage());
             return "";
         }
-    }
+    } // fetchPage
 
-    private pageInfo splitPage( String page, String url ) {
+    private pageInfo splitPage(String page, String url) {
         pageInfo pi = new pageInfo();
-        pi.content=page; // save it all for future reference
+        pi.links = new Vector<String>();
+        pi.content = page; // save it all for future reference
         pi.url = url;
         // Split headers and body, if possible
-        Pattern p1 = Pattern.compile("<head>(.*)</head>.*"+
-                "<body>(.*)", 
+        Pattern p1 = Pattern.compile("<head>(.*)</head>.*" +
+                "<body[^>]*>(.*)",
                 Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
         Matcher m1 = p1.matcher(page);
         if (m1.find()) {
-            logger.log(Level.TRACE, "P1 matched." );
+            //logger.log(Level.TRACE, "P1 matched.");
             pi.headers = m1.group(1);
             pi.body = m1.group(2);
         } else {
-            logger.log(Level.TRACE, "P1 did NOT match. p='" + p1.pattern() +"'" );
-            pi.headers ="";
+            logger.log(Level.TRACE, "P1 did NOT match. p='" + p1.pattern() + "'");
+            pi.headers = "";
             pi.body = page; // doesn't look like good html, try to extract links anyway
         }
         // Extract a title
-        pi.title="";
-        Pattern p2 = Pattern.compile("<title>(.*)</title>",
+        pi.title = "";
+        Pattern p2 = Pattern.compile("<title>\\s*(.*\\S)\\s*</title>",
                 Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
         Matcher m2 = p2.matcher(pi.headers);
         if (m2.find()) {
-            pi.title=m2.group(1);
-        } else {            
-            pi.title="???"; // FIXME - try to get the first H1 tag, 
-            // or first text line or something
+            pi.title = m2.group(1);
+        } else {
+            pi.title = "???"; // FIXME - try to get the first H1 tag, 
+        // or first text line or something
         }
         // extract full text
 
@@ -210,65 +202,109 @@ public class WebHarvestJob implements HarvestJob {
         }
 
         //Pattern p3 = Pattern.compile("<a([^>]+)>",
-        Pattern p3 = Pattern.compile("<a[^>]+href=['\"]?([^>'\"#]+)['\"# ]?[^>]*>", 
+        Pattern p3 = Pattern.compile("<a[^>]+href=['\"]?([^>'\"#]+)['\"# ]?[^>]*>",
                 Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
         Matcher m3 = p3.matcher(pi.body);
         while (m3.find()) {
             String lnk = m3.group(1);
-            
+
             URL linkUrl;
             try {
                 linkUrl = new URL(pageUrl, lnk);
             } catch (MalformedURLException ex) {
-                logger.log(Level.TRACE, "Could not make a good url from '" + lnk +"'");
+                logger.log(Level.TRACE, "Could not make a good url from '" + lnk + "'");
                 break;
             }
             String newlnk = linkUrl.toString();
-            logger.log(Level.TRACE, "Found link '" + m3.group() +"' '"+lnk+"' "+
-                    "-> '" + newlnk + "'" );
-            
+            //logger.log(Level.TRACE, "Found link '" + m3.group() + "' '" + lnk + "' " +
+            //        "-> '" + newlnk + "'");
+            if (!pi.links.contains(newlnk)) {
+                pi.links.add(newlnk);
+            }
         }
-        
-        logger.log(Level.TRACE, "Split page " + pi.url + " into "+
-                "title:'" + pi.title + "'  " +
-                "headers (" + pi.headers.length() + ") " +
-                "body: (" + pi.body.length() + ") " );
 
-        
+        logger.log(Level.DEBUG, 
+                resource.getName() +
+                " " + round +":" +searched.size() +"/" + numtosearch +
+                " " + pi.url + " " +
+                "title:'" + pi.title + "' (" +
+                "h=" + pi.headers.length() + "b " +
+                "b=" + pi.body.length() + "b) " +
+                pi.links.size() + " links");
+
+
         return pi;
     }
-    
-    private void harvestLoop() {
-        while (!toSearch.isEmpty()) {
+
+    private void initWorkList() {
+        searched = new Vector<String>();
+        toSearch = new Vector<String>();
+        nextRound = new Vector<String>();
+        for (String startUrl : resource.getStartUrls().split(" ")) {
+            toSearch.add(startUrl);
+            logger.log(Level.TRACE, "Added start URL '" + startUrl + "'");
+        // Fixme - validate they all are OK
+        }
+        numtosearch = toSearch.size();
+    }
+
+    /** Harvest one round of links */
+    private void harvestRound() {
+        while (!toSearch.isEmpty() && !isKillSendt()) {
             String curUrl = toSearch.firstElement();
             toSearch.removeElementAt(0);
-            if ( !searched.contains(curUrl)) {
+            if (!searched.contains(curUrl)) {
                 searched.add(curUrl); // to make sure we don't go there again  
                 // Fixme - check robot safe
+                resource.setCurrentStatus("" + searched.size() + "pages visited");
                 String page = fetchPage(curUrl);
                 if (!page.isEmpty()) {
                     pageInfo pi = splitPage(page, curUrl);
+                    for (String u : pi.links) {
+                        if (!nextRound.contains(u)) {
+                            // FIXME - filter those links that need to be followed
+                            nextRound.add(u);
+                        }
+                        try {
+                            //Thread.sleep(1000); // simple load reduction on the servers
+                            Thread.sleep(100); 
+                        } catch (InterruptedException ex) {
+                            break; // never mind if that sleep got interrupted
+                        }
+                    }
                 }
             }
         }
-    } // harvestLoop
+    } // harvestRound
+
+    /** Harvest all there is to do */
+    private void harvestLoop() {
+        round = 0;
+        initWorkList();
+        while (round <= resource.getRecursionDepth() && !isKillSendt()) {
+            harvestRound();
+            logger.log(Level.DEBUG, "Round " + round + ": " +
+                    searched.size() + " urls seen. " +
+                    nextRound.size() + " urls to go ");
+            toSearch.addAll(nextRound);
+            numtosearch += toSearch.size();
+            nextRound.clear();
+            round++;
+        }
+        logger.log(Level.DEBUG, "Did " + (round - 1) + " rounds. " +
+                "(max=" + resource.getRecursionDepth() + ") " +
+                "(iskilled=" + isKillSendt() + ") " +
+                "Seen " + searched.size() + " urls");
+    }
 
     public void run() {
         status = HarvestStatus.RUNNING;
-        initWorkList();
         harvestLoop();
-        resource.setCurrentStatus("Sleeping");
-        try {
-            resource.setCurrentStatus("Sleeping");
-            Thread.sleep(10000);
-            resource.setCurrentStatus("Woke up");
-        } catch (InterruptedException ex) {
-            setError("Web crawler didn't even manage to sleep!");
-        }
         if (isKillSendt()) {
             setError("Web Crawl interrupted with a kill signal");
         } else {
-            setError("Web Crawl not quite implemented yet");
+            //status = HarvestStatus.FINISHED;
+            setError("All done - but we call it an error so we can do again");
         }
     } // run()
 }
