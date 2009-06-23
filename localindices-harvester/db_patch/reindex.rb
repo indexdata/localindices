63cf8143d9f023c030fc2e2d21c594f8429b3824
Jakub Skoczen <jakub@indexdata.dk>
2009-06-23 16:21:26 +0200

    Reindexing will not overwrite old index anymore.

--- localindices-harvester/db_patch/reindex.rb
+++ localindices-harvester/db_patch/reindex.rb
@@ -8,17 +8,18 @@
 require 'dbi'
 
 commited_path = "/var/cache/harvested/committed"
+conf_name = "zebra-reidx.cfg"
 
 begin
-  print "asadmin stop-domain domain1 \n"
-  system "asadmin stop-domain domain1 "
+  #print "asadmin stop-domain domain1 \n"
+  #system "asadmin stop-domain domain1 "
   dbh = DBI.connect('DBI:MySQL:localindices:localhost',
   'localidxadm', 'localidxadmpass')
 
   print "\n"
   print "zebraidx init"
   print "\n"
-  system "zebraidx init"
+  system "zebraidx -c #{conf_name} init"
 
   sth = dbh.prepare("SELECT ID, DTYPE, METADATAPREFIX FROM HARVESTABLE")
   sth.execute
@@ -39,7 +40,7 @@ begin
         dom_conf = "pz-pz.xml"
     end
     upd_stmt = 
-      "zebraidx -tdom.#{dom_conf} -d #{zdb_name} -l #{zdb_name}.log update #{commited_path}/#{zdb_name}"
+      "zebraidx -c #{conf_name} -tdom.#{dom_conf} -d #{zdb_name} -l #{zdb_name}.log update #{commited_path}/#{zdb_name}"
     print upd_stmt + "\n"
     system upd_stmt
   end
@@ -48,10 +49,10 @@ begin
   print "\n"
   print "zebraidx commit"
   print "\n"
-  
-  system "zebraidx commit"
-  print "asadmin start-domain domain1 \n"
-  system "asadmin start-domain domain1 "
+ 
+  system "zebraidx -c #{conf_name} commit"
+  #print "asadmin start-domain domain1 \n"
+  #system "asadmin start-domain domain1 "
 
 rescue DBI::DatabaseError => e
   puts "Error: #{e.message}"
