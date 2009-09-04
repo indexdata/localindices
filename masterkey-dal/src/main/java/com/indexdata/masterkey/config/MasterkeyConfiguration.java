7ff1ba3146c3b847781b8b9c3dbd620ceda94f86
Niels Erik Nielsen <nielserik@indexdata.com>
2009-09-04 12:12:21 -0400

    Re-instates config methods to fix Torus

--- masterkey-dal/src/main/java/com/indexdata/masterkey/config/MasterkeyConfiguration.java
+++ masterkey-dal/src/main/java/com/indexdata/masterkey/config/MasterkeyConfiguration.java
@@ -30,14 +30,16 @@ public class MasterkeyConfiguration {
 
     private Logger logger = Logger.getLogger("com.indexdata.masterkey.config.");
     private static ConcurrentHashMap<String,MasterkeyConfiguration> configLocationCache = new ConcurrentHashMap();
-    private boolean cacheConfigParams = true;    
+    private boolean cacheConfigParams = true;
+    private String serviceName = null;
 
     private ConcurrentHashMap<String,Properties> configParametersCache = new ConcurrentHashMap();
     ConfigFileLocation configFileLocation = null;
 
-    private MasterkeyConfiguration(String servletName, ServletContext servletContext, String hostName) throws IOException {
+    private MasterkeyConfiguration(String serviceName, ServletContext servletContext, String hostName) throws IOException {
         cacheConfigParams = areConfigParamsCached(servletContext.getInitParameter(MASTERKEY_CONFIG_LIFE_TIME_PARAM));
         configFileLocation = new ConfigFileLocation(servletContext, hostName);
+        this.serviceName = serviceName;
     }
 
     private Logger getLogger() {
@@ -45,25 +47,27 @@ public class MasterkeyConfiguration {
     }
 
     /**
-     * Creates a singleton MasterkeyCoservletContext.getRnfiguration for each combination of component and host name.
+     * Creates a singleton MasterkeyConfiguration for each combination of component and host name.
      * The instance acts as a cache for the location of the specific config file, but is not
-     * a cache forthe configuration parameters as such.
+     * a cache for the configuration parameters as such.
      * In other words: The location of the config file for a given component in a given host is
      * retrieved once in the servlets life time, how often the configuration parameters are retrieved
      * is configurable
-     * @param servletName An identifier for this configuration. Must match the prefix for 
-     *        the properties in the property file, like AuthServlet.my_property
+     * @param serviceName An identifier for this configuration. Must match the prefix for the properties in the property file, 
+     *                    like AuthServlet.my_property or RecordsResource.my_property
+     *                    Is typically a Servlet name but if identification beyond Servlet name
+     *                    is required - i.e. for REST services - then any other identifier can be used.
      * @param servletContext Needed to pick up init parameters regarding the location of config files
      * @param hostName Used for resolving the path to config files.
      */
-    public static MasterkeyConfiguration getInstance (String servletName, ServletContext servletContext, String hostName) throws IOException {
+    public static MasterkeyConfiguration getInstance (String serviceName, ServletContext servletContext, String hostName) throws IOException {
         MasterkeyConfiguration cfg = null;
-        String cfgKey = servletContext.getContextPath() + "/" + servletName +"@"+hostName;
+        String cfgKey = servletContext.getContextPath() + "/" + serviceName +"@"+hostName;
         if (configLocationCache.containsKey(cfgKey)) {
             cfg = (MasterkeyConfiguration) (configLocationCache.get(cfgKey));
             cfg.getLogger().debug("Returning cached config location for '" + cfgKey + "': '" + cfg.getConfigFileLocation().getConfigFilePath() + "'");
         } else {            
-            cfg = new MasterkeyConfiguration(servletName, servletContext, hostName);
+            cfg = new MasterkeyConfiguration(serviceName, servletContext, hostName);
             cfg.getLogger().debug("No previously cached config location reference found for '" + cfgKey + "'. Instantiating a new config location reference: '" + cfg.getConfigFileLocation().getConfigFilePath() + "'");
             // Check that config file is readable, if not, analyze and throw exception
             cfg.getConfigFileLocation().evaluate();
@@ -72,7 +76,8 @@ public class MasterkeyConfiguration {
         }
         return cfg;
     }
-
+    
+    
     /**
      * Sets the life time of configuration parameters to 'servlet' (cache=true) or 'request' (cache=false
      * @param configLifeTime
@@ -117,6 +122,27 @@ public class MasterkeyConfiguration {
     }
 
     /**
+     * Retrieves all config paramers prefixed with the given service name (ie the servlet name) 
+     * @return
+     * @throws IOException
+     */
+    private Enumeration getConfigParameterNames() throws IOException {
+        Properties prop = getComponentProperties(configFileLocation.getConfigFilePath());
+        Hashtable<String, String> keyList = new Hashtable<String, String>();
+        Iterator keysIter = prop.keySet().iterator();
+        int i = 0;
+        while (keysIter.hasNext()) {
+            String key = (String) keysIter.next();
+            if (key.startsWith(serviceName)) {
+                key = key.replace(serviceName + ".", "");
+                keyList.put("" + (i++), key);
+            }
+        }
+        return keyList.elements();
+    }
+    
+    
+    /**
      * Retrieves a given init parameter that applies to the invoking Servlet.
      * @param name
      * @return
@@ -133,6 +159,23 @@ public class MasterkeyConfiguration {
         return propertyValue;
     }
 
+    /**
+     * Retrieves a given init parameter prefixed with a given service name (ie a servlet name) 
+     * @param name  The name of the parameter, without the service name prefix.
+     * @return
+     * @throws javax.servlet.ServletException
+     */
+    public String getConfigParameter(String name) throws IOException {
+        Properties prop = getComponentProperties(configFileLocation.getConfigFilePath());
+        String propertyValue = (String) prop.get(serviceName + "." + name);
+        if (propertyValue == null || propertyValue.length() == 0) {
+            logger.error("Could not find value for key '" + name + "'");
+        } else {
+            logger.debug("Found value '" + propertyValue + "' for key '" + name + "'");
+        }
+        return propertyValue;
+    }
+    
 
     /**
      * Retrieves properties (configuration) for a given J2EE module
