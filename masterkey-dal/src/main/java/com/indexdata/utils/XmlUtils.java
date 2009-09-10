9f3e3246616708cfd7ee535a8f7379af62ffc698
Jakub Skoczen <jakub@indexdata.dk>
2009-09-10 16:24:41 +0200

    Added.

--- masterkey-dal/src/main/java/com/indexdata/utils/XmlUtils.java
+++ masterkey-dal/src/main/java/com/indexdata/utils/XmlUtils.java
@@ -22,6 +22,7 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -49,6 +50,13 @@ public class XmlUtils {
             transformer = factory.newTransformer();
         }
         return transformer;
+    }
+
+    public static Document newDoc(String rootNode) throws ParserConfigurationException {
+        Document doc = getBuilder().newDocument();
+        Element root = doc.createElement(rootNode);
+        doc.appendChild(root);
+        return doc;
     }
     
     public static Document parse(InputStream source) 
