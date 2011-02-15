56940448597fabd2143fc0791093f1ddb26fd8f5
Dennis Schafroth <dennis@indexdata.com>
2011-02-15 23:31:40 +0100

    Test SolrXmlBulkResource

---
+++ masterkey-dal/src/main/java/com/indexdata/masterkey/localindices/entity/SolrXmlBulkResource.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 1995-2008, Index Data
+ * All rights reserved.
+ * See the file LICENSE for details.
+ */
+
+package com.indexdata.masterkey.localindices.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author jakub
+ */
+@Entity
+@XmlRootElement(name="xmlBulk")
+public class SolrXmlBulkResource extends Harvestable implements Serializable {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -1871398509135614281L;
+	@Column(length=16384)
+    private String url;
+    private String expectedSchema;
+    private String normalizationFilter;
+
+    public String getExpectedSchema() {
+        return expectedSchema;
+    }
+
+    public void setExpectedSchema(String expectedSchema) {
+        this.expectedSchema = expectedSchema;
+    }
+
+    public String getNormalizationFilter() {
+        return normalizationFilter;
+    }
+
+    public void setNormalizationFilter(String normalizationFilter) {
+        this.normalizationFilter = normalizationFilter;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof SolrXmlBulkResource)) {
+            return false;
+        }
+        SolrXmlBulkResource other = (SolrXmlBulkResource) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+}
