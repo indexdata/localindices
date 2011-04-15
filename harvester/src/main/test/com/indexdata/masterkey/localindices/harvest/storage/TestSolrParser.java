a066f0e5e36e22c956a30c7b2a33e5256c2726da
Dennis Schafroth <dennis@indexdata.com>
2011-04-15 14:49:29 +0200

    Test of Solr xml parsing and storage.

---
+++ harvester/src/main/test/com/indexdata/masterkey/localindices/harvest/storage/TestSolrParser.java
@@ -0,0 +1,75 @@
+package com.indexdata.masterkey.localindices.harvest.storage;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.apache.solr.common.SolrInputDocument;
+
+import junit.framework.TestCase;
+
+public class TestSolrParser extends TestCase 
+{
+	String xml 
+		  = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + 
+			"<add>" +
+			"  <doc>\n" + 
+			"      <field name=\"id\">6688</field>\n" + 
+			"      <field name=\"author\">Eliot, George, 1819-1880</field>\n" + 
+			"      <field name=\"author-title\"></field>\n" + 
+			"      <field name=\"author-date\"/>\n" + 
+			"      <field name=\"title\">The Mill on the Floss</field>\n" + 
+			"      <field name=\"medium\">book</field>\n" +
+			"      <field other=\"author-date\">Value</field>\n" + 
+			"      <other name=\"other\"></other>\n" +
+			"   </doc>\n" + 
+			"  <doc>\n" + 
+			"      <field name=\"id\">6689</field>\n" +
+			"      <field name=\"author\">Eliot, George, 1819-1880</field>\n" + 
+			"      <field name=\"author-title\"></field>\n" + 
+			"      <field name=\"author-date\"/>\n" + 
+			"      <field name=\"title\">The Mill on the Floss</field>\n" + 
+			"      <field name=\"medium\">book</field>\n" +
+			"      <field other=\"author-date\">Value</field>\n" + 
+			"      <other name=\"other\"></other>\n" +
+			"   </doc>\n" + 
+			"</add>";
+	
+	public void testSolrParse() throws IOException 
+	{
+		SolrXmlParser parser = new SolrXmlParser();
+		XmlHandler context = new PrintXmlHandler();
+		try {
+			parser.parse(xml, context);
+		} catch (XMLStreamException e) {
+			e.printStackTrace();
+		}
+
+		SolrXmlHandler solrContext = new SolrXmlHandler();
+		try {
+			parser.parse(xml, solrContext);
+		} catch (XMLStreamException e) {
+			e.printStackTrace();
+		}
+		Collection<SolrInputDocument> documents = solrContext.getDocuments();
+		assertTrue(documents.size() == 2); 
+		Iterator<SolrInputDocument> iterator = documents.iterator();
+		while (iterator.hasNext()) {
+			SolrInputDocument doc = iterator.next();
+			System.out.print(doc.entrySet());
+		}
+		try {
+			parser.parse(xml, solrContext);
+		} catch (XMLStreamException e) {
+			e.printStackTrace();
+		}
+		while (iterator.hasNext()) {
+			SolrInputDocument doc = iterator.next();
+			System.out.print(doc.entrySet());
+		}
+	}
+
+
+}
---
+++ harvester/src/main/test/com/indexdata/masterkey/localindices/harvest/storage/TestSolrStorage.java
@@ -0,0 +1,66 @@
+package com.indexdata.masterkey.localindices.harvest.storage;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+
+import junit.framework.TestCase;
+
+import com.indexdata.masterkey.localindices.entity.Harvestable;
+import com.indexdata.masterkey.localindices.entity.SolrXmlBulkResource;
+import com.indexdata.masterkey.localindices.harvest.storage.HarvestStorage;
+
+public class TestSolrStorage extends TestCase {
+	Harvestable harvestable = new  SolrXmlBulkResource();
+	HarvestStorage storage = new SolrStorage(harvestable);
+	String testXml 
+	  = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" +
+		"<add>" +
+		"  <doc>\n" + 
+		"      <field name=\"id\">6688</field>\n" + 
+		"      <field name=\"author\">Eliot, George, 1819-1880</field>\n" + 
+		"      <field name=\"author-title\"></field>\n" + 
+		"      <field name=\"author-date\"/>\n" + 
+		"      <field name=\"title\">The Mill on the Floss</field>\n" + 
+		"      <field name=\"medium\">book</field>\n" +
+		"      <field other=\"author-date\">Value</field>\n" + 
+		"      <other name=\"other\"></other>\n" +
+		"   </doc>\n" + 
+		"  <doc>\n" + 
+		"      <field name=\"id\">6689</field>\n" +
+		"      <field name=\"author\">Eliot, George, 1819-1880</field>\n" + 
+		"      <field name=\"author-title\"></field>\n" + 
+		"      <field name=\"author-date\"/>\n" + 
+		"      <field name=\"title\">The Mill on the Floss</field>\n" + 
+		"      <field name=\"medium\">book</field>\n" +
+		"      <field other=\"author-date\">Value</field>\n" + 
+		"      <other name=\"other\"></other>\n" +
+		"   </doc>\n" + 
+		"</add>";
+	
+	
+	public TestSolrStorage() {
+		
+	}
+	public void testSimplePost() throws IOException 
+	{
+		SolrXmlHandler context = new SolrXmlHandler();
+		SolrXmlParser parser = new SolrXmlParser();
+		try {
+			parser.parse(testXml, context);
+			storage.begin();
+			OutputStream output = storage.getOutputStream();
+			Writer writer = new OutputStreamWriter(output);
+			writer.write(testXml);
+			writer.close();
+			storage.commit();
+		} catch (XMLStreamException e) {
+			System.out.println("Failure to parse");
+		}
+	}
+}
