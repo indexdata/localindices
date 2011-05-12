a8c766e4d88ced891715c54135f7a49a5ac5ab21
Dennis Schafroth <dennis@indexdata.com>
2011-05-12 12:33:33 +0200

    Changed according to API

--- harvester/src/main/java/com/indexdata/masterkey/localindices/step/XslTransformationStep.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/step/XslTransformationStep.java
@@ -1,22 +1,17 @@
 package com.indexdata.masterkey.localindices.step;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
 import java.io.StringReader;
-import java.io.Writer;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.FactoryConfigurationError;
-import javax.xml.stream.XMLOutputFactory;
-import javax.xml.stream.XMLStreamException;
+import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stax.StAXResult;
+import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.stream.StreamSource;
 
 public class XslTransformationStep implements TransformationStep, Step {
@@ -35,23 +30,22 @@ public class XslTransformationStep implements TransformationStep, Step {
 		transformer = factory.newTransformer(new StreamSource(new StringReader(this.xslTransformation)));  
 	};
 	
-	public TransformResult transform(InputStream input, OutputStream output) throws IOException 
+	public TransformResult transform(Source input, MessageConsumer output) throws IOException 
 	{
 		if (transformer != null) {
-			Writer writer = new OutputStreamWriter(output); 
-			Reader reader = new InputStreamReader(input);
 			try {
-				transformer.transform(new StreamSource(reader), new StAXResult(XMLOutputFactory
-				        .newInstance().createXMLStreamWriter(writer)));
+				DOMResult result = new DOMResult(DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument()); 
+				transformer.transform(input, result);
+				output.accept(new DOMResultVisitor(result));
 			} catch (TransformerException e) {
 				e.printStackTrace();
 				throw new IOException("Transformation error", e);
-			} catch (XMLStreamException e) {
-				e.printStackTrace();
-				throw new IOException("XMLStreamException error", e);
 			} catch (FactoryConfigurationError e) {
 				e.printStackTrace();
 				throw new IOException("FactoryConfiguration error", e);
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 			return new TransformResult() {
 				/* TODO Implement */
