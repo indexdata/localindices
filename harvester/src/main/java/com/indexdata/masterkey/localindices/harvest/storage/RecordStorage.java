e3f36c3e536ee8a6733d67863c8fd54f7928ef05
Dennis Schafroth <dennis@indexdata.com>
2011-05-12 12:22:34 +0200

    Renamed RestoreStore

---
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/RecordStorage.java
@@ -0,0 +1,17 @@
+package com.indexdata.masterkey.localindices.harvest.storage;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Map;
+
+public interface RecordStorage extends HarvestStorage {
+	
+	void databaseStart(Map<String ,String> properties);
+	void databaseEnd();
+	void add(Map<String, Collection<Serializable>> keyValues);
+	void add(Record record);
+	Record get(String id);
+	// void remove(Collection<String> ids);
+	void remove(String ids);
+
+}
