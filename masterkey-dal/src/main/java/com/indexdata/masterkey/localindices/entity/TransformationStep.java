fdc81bb5dd1125e76a7c8e443458c43044be315a
Dennis Schafroth <dennis@indexdata.com>
2011-05-23 16:02:14 +0200

    Transformation Pipeline DAO

---
+++ masterkey-dal/src/main/java/com/indexdata/masterkey/localindices/entity/TransformationStep.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 1995-2008, Index Data
+ * All rights reserved.
+ * See the file LICENSE for details.
+ */
+
+package com.indexdata.masterkey.localindices.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+/**
+ * @author Dennis
+ */
+@Entity
+@NamedQueries({@NamedQuery(name = "TransformationStep.findById", query = "SELECT o FROM TransformationStep o WHERE o.id = :id")})
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+public abstract class TransformationStep implements Serializable, Cloneable {
+
+    protected static final long serialVersionUID = 1L;
+    // user-set properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected Long id;
+    protected String name;
+    @Column(length=4096)
+    protected String description;
+    protected Boolean enabled;
+    @Column(length=100000)
+    protected String script;
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+
+    public String getScript() {
+        return script;
+    }
+
+    public void setScript(String script) {
+        this.script = script;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // change that so that it check the real class
+        if (!(object instanceof Transformation)) {
+            return false;
+        }
+        Transformation other = (Transformation) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getCanonicalName() + "[id=" + id + "]";
+    }
+    
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
