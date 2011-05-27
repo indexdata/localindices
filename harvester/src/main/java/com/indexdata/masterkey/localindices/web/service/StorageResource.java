f7c16d9be7ccbac1bbdcac91edd51f3e578ac613
Dennis Schafroth <dennis@indexdata.com>
2011-05-27 10:21:27 +0200

    Storage, Transformation Resources

---
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/web/service/StorageResource.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 1995-2008, Index Data
+ * All rights reserved.
+ * See the file LICENSE for details.
+ */
+
+package com.indexdata.masterkey.localindices.web.service;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.UriInfo;
+
+import com.indexdata.masterkey.localindices.dao.StorageDAO;
+import com.indexdata.masterkey.localindices.dao.bean.StoragesDAOJPA;
+import com.indexdata.masterkey.localindices.entity.Storage;
+import com.indexdata.masterkey.localindices.web.service.converter.StorageConverter;
+
+/**
+ * REST Web service (resource) that maps to a Storage entity.
+ * @author jakub
+ */
+public class StorageResource {
+    private StorageDAO dao = new StoragesDAOJPA();
+    private Long id;
+    private UriInfo context;
+
+    /** Creates a new instance of HarvestableResource */
+    public StorageResource() {
+    }
+
+    /**
+     * Constructor used for instantiating an instance of the entity referenced by id.
+     *
+     * @param id identifier for referenced the entity
+     * @param context HttpContext inherited from the parent resource
+     */
+    public StorageResource(Long id, UriInfo context) {
+        this.id = id;
+        this.context = context;
+    }
+
+    /**
+     * Get method for retrieving an instance of referenced Storage in XML format.
+     *
+     * @return an instance of StorageConverter
+     */
+    @GET
+    @Produces("application/xml")
+    public StorageConverter get() {
+        return new StorageConverter(dao.retrieveStorageById(id), context.getAbsolutePath());
+    }
+
+    /**
+     * Put method for updating an instance of refernced Harvestable, using XML as the input format.
+     *
+     * @param data an HarvestableConverter entity that is deserialized from an XML stream
+     */
+    @PUT
+    @Consumes("application/xml")
+    public void put(StorageConverter data) {
+        Storage entity = data.getEntity();
+        // TODO Fix
+        entity.setCurrentStatus("NEW");
+        entity.setMessage(null);
+        dao.updateStorage(entity);
+    }
+
+    /**
+     * Delete method for deleting an instance of referenced Harvestable.
+     *
+     */
+    @DELETE
+    public void delete() {
+        dao.deleteStorage(dao.retrieveStorageById(id));
+    }
+
+}
---
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/web/service/StoragesResource.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 1995-2008, Index Data
+ * All rights reserved.
+ * See the file LICENSE for details.
+ */
+
+package com.indexdata.masterkey.localindices.web.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import com.indexdata.masterkey.localindices.dao.StorageDAO;
+import com.indexdata.masterkey.localindices.dao.bean.StoragesDAOJPA;
+import com.indexdata.masterkey.localindices.entity.Storage;
+import com.indexdata.masterkey.localindices.web.service.converter.StorageConverter;
+import com.indexdata.masterkey.localindices.web.service.converter.StoragesConverter;
+
+/**
+ * RESTful WS (resource) that maps to the Storage collection.
+ * @author jakub
+ */
+@Path("/storages/")
+public class StoragesResource {
+    private StorageDAO dao = new StoragesDAOJPA();
+    @Context
+    private UriInfo context;
+
+    /** Creates a new instance of StoragesResource */
+    public StoragesResource() {
+    }
+
+    /**
+     * Constructor used for instantiating an instance of the Storages resource.
+     *
+     * @param context HttpContext inherited from the parent resource
+     */
+    public StoragesResource(UriInfo context) {
+        this.context = context;
+    }
+
+    /**
+     * Get method for retrieving a collection of Storage instance in XML format.
+     *
+     * @param start optional start item argument
+     * @param max optional max results argument
+     * @return an instance of StorageConverter
+     */
+    @GET
+    @Produces("application/xml")
+    public StoragesConverter get(
+            
+            @QueryParam("start")
+            @DefaultValue("0") int start,
+            
+            @QueryParam("max")
+            @DefaultValue("100") int max) {
+        List<Storage> entities;
+        if (max <= 0)
+            entities = new ArrayList<Storage>();
+        else
+            entities = dao.retrieveStorages(start, max);
+        return new StoragesConverter(entities, context.getAbsolutePath(),
+                start, max, dao.getStorageCount());
+    }
+
+    /**
+     * Post method for creating an instance of Storage using XML as the input format.
+     *
+     * @param data an StorageConverter entity that is deserialized from an XML stream
+     * @return Http 201 response code.
+     */
+    @POST
+    @Consumes("application/xml")
+    public Response post(StorageConverter data) {
+        Storage entity = data.getEntity();
+        entity.setCurrentStatus("NEW");
+        dao.createStorage(entity);
+        return Response.created(context.getAbsolutePath().resolve(entity.getId() + "/")).build();
+    }
+
+    /**
+     * Entry point to the Storage WS.
+     *
+     * @param id resource id
+     * @return an instance of StorageResource (WS)
+     */
+    @Path("{id}/")
+    public StorageResource getStorageResource(            
+    @PathParam("id") Long id) {
+        return new StorageResource(id, context);
+    }
+}
