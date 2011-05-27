18f51bd177781b54c1eb53ae46b462e812ec5f94
Dennis Schafroth <dennis@indexdata.com>
2011-05-27 12:09:56 +0200

    New entities for Transformation and Steps

---
+++ masterkey-dal/src/main/java/com/indexdata/masterkey/localindices/entity/BasicTransformation.java
@@ -0,0 +1,17 @@
+package com.indexdata.masterkey.localindices.entity;
+
+import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@Entity
+@XmlRootElement(name = "basicTransformation")
+public class BasicTransformation extends Transformation {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1789712151621170759L;
+	
+
+}
---
+++ masterkey-dal/src/main/java/com/indexdata/masterkey/localindices/entity/BasicTransformationStep.java
@@ -0,0 +1,15 @@
+package com.indexdata.masterkey.localindices.entity;
+
+import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement(name = "basicTransformStep")
+public class BasicTransformationStep extends TransformationStep {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4552043105886552958L;
+
+}
