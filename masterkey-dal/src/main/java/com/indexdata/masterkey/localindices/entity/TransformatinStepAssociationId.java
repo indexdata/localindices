118f7d6f3acee68f260483e0c07b65d3b710b71b
Dennis Schafroth <dennis@indexdata.com>
2011-06-17 17:23:58 -0400

    Misspelled. Must be deleted

---
+++ masterkey-dal/src/main/java/com/indexdata/masterkey/localindices/entity/TransformatinStepAssociationId.java
@@ -0,0 +1,21 @@
+package com.indexdata.masterkey.localindices.entity;
+
+public class TransformatinStepAssociationId {
+	
+	private Long transformationId;
+	private Long stepId;
+	
+	public Long getTransformationId() {
+		return transformationId;
+	}
+	public void setTransformationId(Long transformationId) {
+		this.transformationId = transformationId;
+	}
+	public Long getStepId() {
+		return stepId;
+	}
+	public void setStepId(Long stepId) {
+		this.stepId = stepId;
+	}
+
+}
