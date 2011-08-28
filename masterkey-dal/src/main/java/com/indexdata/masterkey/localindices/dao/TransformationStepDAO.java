3a0fe239ffe58e5b56b264c05f1519cbe5705fc7
Dennis Schafroth <dennis@indexdata.com>
2011-08-28 22:44:52 +0200

    Unify API

--- masterkey-dal/src/main/java/com/indexdata/masterkey/localindices/dao/TransformationStepDAO.java
+++ masterkey-dal/src/main/java/com/indexdata/masterkey/localindices/dao/TransformationStepDAO.java
@@ -1,30 +1,9 @@
 package com.indexdata.masterkey.localindices.dao;
 
-import java.util.List;
-
 import com.indexdata.masterkey.localindices.entity.TransformationStep;
 import com.indexdata.masterkey.localindices.web.service.converter.TransformationStepBrief;
 
-public interface TransformationStepDAO {
+public interface TransformationStepDAO extends CommonDAO<TransformationStep, TransformationStepBrief> 
+{
 
-	/* CRUD */ 
-	public void create(TransformationStep entity);
-    public TransformationStep retrieveById(Long id);
-    public TransformationStep update(TransformationStep entity);
-    public void delete(TransformationStep entity);
-    List<TransformationStep> retrieve(int start, int max);
-
-    /**
-     * Retrieve a list of brief entities.
-     * @return
-     */
-    List<TransformationStepBrief> retrieveBriefs(int start, int max);
-    /**
-     * Retrieves a Step using it's listing reference (brief)
-     * @param hbrief brief (listing) Step
-     * @return TransformationStep detailed 
-     */
-    TransformationStep retrieveFromBrief(TransformationStepBrief tbrief);
-    
-    int getCount();
 }
