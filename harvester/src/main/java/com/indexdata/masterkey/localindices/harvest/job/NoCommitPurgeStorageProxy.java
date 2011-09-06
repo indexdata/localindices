26398fe9b97e67cc72b36444b1d94f8b97016eb2
Dennis Schafroth <dennis@indexdata.com>
2011-09-06 16:27:52 +0200

    Not used. By-pass Commit

---
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/job/NoCommitPurgeStorageProxy.java
@@ -0,0 +1,24 @@
+package com.indexdata.masterkey.localindices.harvest.job;
+
+import org.apache.log4j.Logger;
+
+import com.indexdata.masterkey.localindices.harvest.storage.RecordStorage;
+import com.indexdata.masterkey.localindices.harvest.storage.RecordStorageProxy;
+
+public class NoCommitPurgeStorageProxy extends RecordStorageProxy  {
+
+	private Logger logger = Logger.getLogger(getClass());
+	
+	public NoCommitPurgeStorageProxy(RecordStorage storage) {
+		setTarget(storage);
+	}
+	
+	public void purge() {
+		logger.info("purge is being ignored.");
+	}
+
+	public void commit() {
+		logger.info("commit is being ignored.");
+	}
+	
+}
