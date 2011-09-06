80dedafcc7a1bc2a242bb2e93a33e2e8c7127c5f
Dennis Schafroth <dennis@indexdata.com>
2011-09-06 16:26:22 +0200

    Indent

--- masterkey-dal/src/main/java/com/indexdata/masterkey/localindices/entity/BasicTransformationStep.java
+++ masterkey-dal/src/main/java/com/indexdata/masterkey/localindices/entity/BasicTransformationStep.java
@@ -7,18 +7,15 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "basicTransformStep")
 public class BasicTransformationStep extends TransformationStep {
 
-	/**
-	 * 
-	 */
-	
-	public BasicTransformationStep() {
-	}
-	
-	public BasicTransformationStep(String name, String description, String script) {
-		this.name = name;
-		this.description = description;
-		this.script = script;
-	}
-	private static final long serialVersionUID = 4552043105886552958L;
+  public BasicTransformationStep() {
+  }
+
+  public BasicTransformationStep(String name, String description, String script) {
+    this.name = name;
+    this.description = description;
+    this.script = script;
+  }
+
+  private static final long serialVersionUID = 4552043105886552958L;
 
 }
