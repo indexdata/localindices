31ff48b7343188617a0fd41ee2667e606a3628b6
Dennis Schafroth <dennis@indexdata.com>
2011-09-07 17:10:33 +0200

    Indent only

--- harvester/src/main/java/com/indexdata/masterkey/localindices/util/HarvestableLog.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/util/HarvestableLog.java
@@ -16,41 +16,40 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- *
+ * 
  * @author jakub
  */
 public class HarvestableLog {
-    //log file
-    private static File logFile = new File("/var/cache/harvested/harvester.log");
+  // log file
+  private static File logFile = new File("/var/cache/harvested/harvester.log");
 
-    // not used but shows the concept of matching against the whole buffer
-    @SuppressWarnings("unused")
-	private static void readLines(Pattern lp, CharBuffer in, StringBuilder out) {
-        Matcher lm = lp.matcher(in);	// Line matcher
-        int lines = 0;
-        while (lm.find()) {
-            lines++;
-            out.append(lm.group()); 	// The current line
-            if (lm.end() == in.limit())
-                break;
-        }
+  // not used but shows the concept of matching against the whole buffer
+  @SuppressWarnings("unused")
+  private static void readLines(Pattern lp, CharBuffer in, StringBuilder out) {
+    Matcher lm = lp.matcher(in); // Line matcher
+    int lines = 0;
+    while (lm.find()) {
+      lines++;
+      out.append(lm.group()); // The current line
+      if (lm.end() == in.limit())
+	break;
     }
+  }
 
-
-    public static String getHarvestableLog(long jobId) throws FileNotFoundException, IOException {
-        BufferedReader r = new BufferedReader(new FileReader(logFile));
-        //build what we need
-        StringBuilder sb = new StringBuilder(1024);
-        Pattern lp = Pattern.compile("JOB#"+jobId);
-        String line = null;
-        Matcher m = null;
-        while ((line = r.readLine()) != null) {
-            m = lp.matcher(line);
-            if (m.find())
-                sb.append(line + "\n");
-        }
-        r.close();
-        return sb.toString();
+  public static String getHarvestableLog(long jobId) throws FileNotFoundException, IOException {
+    BufferedReader r = new BufferedReader(new FileReader(logFile));
+    // build what we need
+    StringBuilder sb = new StringBuilder(1024);
+    Pattern lp = Pattern.compile("JOB#" + jobId);
+    String line = null;
+    Matcher m = null;
+    while ((line = r.readLine()) != null) {
+      m = lp.matcher(line);
+      if (m.find())
+	sb.append(line + "\n");
     }
+    r.close();
+    return sb.toString();
+  }
 
 }
--- harvester/src/main/java/com/indexdata/masterkey/localindices/util/ProcessUtils.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/util/ProcessUtils.java
@@ -14,75 +14,84 @@ import org.apache.log4j.Logger;
 
 /**
  * Process helper class.
+ * 
  * @author jakub
  */
 public class ProcessUtils {
 
-    /**
-     * Executes a process and returns immediately.
-     * @param cmd a command to be executed
-     * @return executed process
-     * @throws java.io.IOException
-     */
-    public static Process execAndReturn(String[] cmd) throws IOException {
-        Process proc = Runtime.getRuntime().exec(cmd);
-        return proc;
-    }
+  /**
+   * Executes a process and returns immediately.
+   * 
+   * @param cmd
+   *          a command to be executed
+   * @return executed process
+   * @throws java.io.IOException
+   */
+  public static Process execAndReturn(String[] cmd) throws IOException {
+    Process proc = Runtime.getRuntime().exec(cmd);
+    return proc;
+  }
 
-    /**
-     * Executes a given command starts a new process, logs it's output 
-     * and destroys it when tha calling thread is interrupted.
-     * @param cmd command to be executed
-     * @param logger a logger to log to
-     * @return process return value
-     * @throws java.io.IOException
-     */
-    public static int execAndWait(String[] cmd, Logger logger) throws IOException {
-        if ( logger.isDebugEnabled() )
-        {
-            String logcmd = "About to execute command: ";
-            for (String c : cmd) {
-                logcmd += c + " ";
-            }
-            logger.log(Level.DEBUG, logcmd);
-        }
-        Process p = execAndReturn(cmd);
-        logError(p, logger);
-        try {
-            p.waitFor();
-            return p.exitValue();
-        } catch (InterruptedException ie) {
-            logger.log(Level.WARN, "Calling thread was interupted, destroying the process.");
-            p.destroy();
-            return -1;
-        }
+  /**
+   * Executes a given command starts a new process, logs it's output and
+   * destroys it when tha calling thread is interrupted.
+   * 
+   * @param cmd
+   *          command to be executed
+   * @param logger
+   *          a logger to log to
+   * @return process return value
+   * @throws java.io.IOException
+   */
+  public static int execAndWait(String[] cmd, Logger logger) throws IOException {
+    if (logger.isDebugEnabled()) {
+      String logcmd = "About to execute command: ";
+      for (String c : cmd) {
+	logcmd += c + " ";
+      }
+      logger.log(Level.DEBUG, logcmd);
     }
-
-    /**
-     * Logs the process error output in a seperate thread.
-     * @param proc process that prints the output to be logged
-     * @param logger a logger to log to
-     * @return logging thread
-     */
-    public static Thread logError(final Process proc, final Logger logger) {
-        Thread lT = new Thread() {
-
-            @Override
-            public void run() {
-                InputStream is = proc.getErrorStream();
-                InputStreamReader isr = new InputStreamReader(is);
-                BufferedReader br = new BufferedReader(isr);
-                String line;
-                try {
-                    while ((line = br.readLine()) != null) {
-                        logger.log(Level.INFO, line);
-                    }
-                } catch (IOException ex) {
-                    logger.log(Level.WARN, "Logging thread encountered an io error (process died?) and will exit.");
-                }
-            }
-        };
-        lT.start();
-        return lT;
+    Process p = execAndReturn(cmd);
+    logError(p, logger);
+    try {
+      p.waitFor();
+      return p.exitValue();
+    } catch (InterruptedException ie) {
+      logger.log(Level.WARN, "Calling thread was interupted, destroying the process.");
+      p.destroy();
+      return -1;
     }
+  }
+
+  /**
+   * Logs the process error output in a seperate thread.
+   * 
+   * @param proc
+   *          process that prints the output to be logged
+   * @param logger
+   *          a logger to log to
+   * @return logging thread
+   */
+  public static Thread logError(final Process proc, final Logger logger) {
+    Thread lT = new Thread() {
+
+      @Override
+      public void run() {
+	InputStream is = proc.getErrorStream();
+	InputStreamReader isr = new InputStreamReader(is);
+	BufferedReader br = new BufferedReader(isr);
+	String line;
+	try {
+	  while ((line = br.readLine()) != null) {
+	    logger.log(Level.INFO, line);
+	  }
+	} catch (IOException ex) {
+	  logger.log(Level.WARN,
+	      "Logging thread encountered an io error (process died?) and will exit.");
+	}
+      }
+    };
+    lT.start();
+    return lT;
+  }
 }
