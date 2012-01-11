48252f18d05fadecd140819106aa8a98ac173905
Dennis Schafroth <dennis@indexdata.com>
2012-01-11 20:19:29 +0100

    Implement StorageJobLogger

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/job/NoCommitPurgeStorageProxy.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/job/NoCommitPurgeStorageProxy.java
@@ -1,24 +1,31 @@
 package com.indexdata.masterkey.localindices.harvest.job;
 
-import org.apache.log4j.Logger;
-
 import com.indexdata.masterkey.localindices.harvest.storage.RecordStorage;
 import com.indexdata.masterkey.localindices.harvest.storage.RecordStorageProxy;
 
 public class NoCommitPurgeStorageProxy extends RecordStorageProxy {
 
-  private Logger logger = Logger.getLogger(getClass());
-
+  //private Logger logger = Logger.getLogger(getClass());
+  private StorageJobLogger logger; 
+  
   public NoCommitPurgeStorageProxy(RecordStorage storage) {
     setTarget(storage);
   }
 
   public void purge() {
-    logger.info("purge is being ignored.");
+    if (logger != null)
+      logger.info("purge is being ignored.");
   }
 
   public void commit() {
-    logger.info("commit is being ignored.");
+    if (logger != null)
+      logger.info("commit is being ignored.");
+  }
+
+  @Override
+  public void setLogger(StorageJobLogger logger) {
+    this.logger = logger; 
+    
   }
 
 }
