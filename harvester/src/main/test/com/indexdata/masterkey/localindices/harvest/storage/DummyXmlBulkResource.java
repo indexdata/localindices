e80683304dbcccd56cbc5dc54d095a0991bf4e0a
Dennis Schafroth <dennis@indexdata.com>
2012-01-11 20:32:51 +0100

    Dummy implementation that sets basic stuff for testing

---
+++ harvester/src/main/test/com/indexdata/masterkey/localindices/harvest/storage/DummyXmlBulkResource.java
@@ -0,0 +1,21 @@
+package com.indexdata.masterkey.localindices.harvest.storage;
+
+import com.indexdata.masterkey.localindices.entity.XmlBulkResource;
+
+public class DummyXmlBulkResource extends XmlBulkResource {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 7350014678423932105L;
+
+  public DummyXmlBulkResource(String url) { 
+    super(url);
+    setId(1l);
+    setName("Test Harvestable");
+    setStorage(new com.indexdata.masterkey.localindices.entity.SolrStorage());
+    getStorage().setId(1l);
+    getStorage().setName("Test Storage");
+  }
+  
+}
