910bdeb6d5a302182decf03827bc27d0ee5007ff
Dennis Schafroth <dennis@indexdata.com>
2012-02-03 15:22:56 +0100

    Implementation of RecordStorage Status interface

---
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/StorageStatus.java
@@ -0,0 +1,17 @@
+package com.indexdata.masterkey.localindices.harvest.storage;
+
+public interface StorageStatus {
+  /* Returns null on not implemented */
+  enum TransactionState 
+  	{
+    		NoTransaction,
+    		InTransaction, 
+    		Committed
+  }
+  Long getTotalRecords();
+  TransactionState getTransactionState();
+  Long getOutstandingAdds();
+  Long getOutstandingDeletes();
+  Long getAdds();
+  Long getDeletes();
+}
