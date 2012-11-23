9b3d9f2c771f6de341bd75ed0339593d18f38e4e
Dennis Schafroth <dennis@indexdata.com>
2012-11-23 16:21:26 +0100

    Rename file storage entity

---
+++ masterkey-dal/src/main/java/com/indexdata/masterkey/localindices/entity/FileStorageEntity.java
@@ -0,0 +1,10 @@
+package com.indexdata.masterkey.localindices.entity;
+
+public class FileStorageEntity extends Storage {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -4384501844217904421L;
+
+}
