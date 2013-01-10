380bb5feaa9fba25f6c009d62538de88fb44714a
Dennis Schafroth <dennis@indexdata.com>
2013-01-10 20:15:38 +0100

    Console logger in debug

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/job/ConsoleStorageJobLogger.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/job/ConsoleStorageJobLogger.java
@@ -1,6 +1,7 @@
 package com.indexdata.masterkey.localindices.harvest.job;
 
 import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
 
 import com.indexdata.masterkey.localindices.entity.Harvestable;
 import com.indexdata.masterkey.localindices.entity.Storage;
@@ -9,15 +10,17 @@ public class ConsoleStorageJobLogger extends StorageJobLogger {
 
   public ConsoleStorageJobLogger(Class<? extends Object> loggerClass, Harvestable resource) {
     super(loggerClass, resource);
+    logger.setLevel(Level.DEBUG);
   }
 
   public ConsoleStorageJobLogger(Class<? extends Object> loggerClass, Storage resource) {
     super(loggerClass, resource);
+    logger.setLevel(Level.DEBUG);
   }
 
   protected void setupAppender(Class<? extends Object> loggerClass, String logFilename, String type) {
     if (logger.getAppender(logFilename) == null) {
-      logAppender = new ConsoleAppender(layout, logFilename);
+      logAppender = new ConsoleAppender(layout);
       logAppender.setName(logFilename);
       logger.addAppender(logAppender);
     }
