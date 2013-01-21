a4bf1a2630847aa1c7b24f207096cb2cbe057359
Dennis Schafroth <dennis@indexdata.com>
2013-01-21 20:39:57 +0100

    Update to have url as parameter

--- harvester.pl
+++ harvester.pl
@@ -1,10 +1,17 @@
 
 use Net::OAI::Harvester;
 
+#'http://www.intechopen.com/oai/index.php';
+my $url = @ARGV[0]; 
+
+die "No url given" if (!$url);
+
+$set = '';
+$set = @ARGV[0] if ($ARGV[0]);
 
 ## create a harvester for the Library of Congress
 my $harvester = Net::OAI::Harvester->new( 
-    'baseURL' => 'http://oai.artstor.org/oaicatmuseum/OAIHandler'
+    'baseURL' => $url
     );
 
 my $identity = $harvester->identify();
@@ -12,7 +19,7 @@ print "name: ",$identity->repositoryName(),"\n";
 
 ## list all the records in a repository
 
-my $records = $harvester->listRecords( metadataPrefix => 'oai_dc', set => 'SetSSCDelaware,SET1' );
+my $records = $harvester->listRecords( metadataPrefix => 'oai_dc', from => "2008-09-01", 'until' => "2008-09-01");
 my $finished = 0;
 
 while ( ! $finished ) {
@@ -20,7 +27,10 @@ while ( ! $finished ) {
     while ( my $record = $records->next() ) {
 	my $header = $record->header();
 	my $metadata = $record->metadata();
-	print "Identifier: ",$header->identifier(),", Set: ", $header->sets(), ", Title: ",$metadata->title(), ", Url: ", $metadata->identifier(), "\n";
+	print "Identifier: ",$header->identifier(),
+	      ", Set: ", $header->sets(), 
+	      ", Title: ",$metadata->title(), 
+	      ", Url: ", $metadata->identifier(), "\n";
     }
 
     my $rToken = $records->resumptionToken();
