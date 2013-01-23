15f1af3467154aab5d392313f4e6ba30f73c2971
Dennis Schafroth <dennis@indexdata.com>
2013-01-23 11:37:33 +0100

    Implement change in OAI-PMH API

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/job/OAIRecordHarvestJob.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/job/OAIRecordHarvestJob.java
@@ -334,7 +334,7 @@ public class OAIRecordHarvestJob extends AbstractRecordHarvestJob {
   private ListRecords listRecords(String baseURL, String from, String until, String setSpec,
       String metadataPrefix) throws IOException {
     try {
-      return new ListRecords(baseURL, from, until, setSpec, metadataPrefix, proxy);
+      return new ListRecords(baseURL, from, until, setSpec, metadataPrefix, proxy, resource.getEncoding());
     } catch (ResponseParsingException hve) {
       String msg = "ListRecords (" + hve.getRequestURL() + ") failed. " + hve.getMessage();
       logger.log(Level.DEBUG, msg + " Erroneous respponse:\n"
@@ -349,7 +349,7 @@ public class OAIRecordHarvestJob extends AbstractRecordHarvestJob {
 
   private ListRecords listRecords(String baseURL, String resumptionToken) throws IOException {
     try {
-      return new ListRecords(baseURL, resumptionToken, proxy);
+      return new ListRecords(baseURL, resumptionToken, proxy, resource.getEncoding());
     } catch (ResponseParsingException hve) {
       String msg = "ListRecords (" + hve.getRequestURL() + ") failed. " + hve.getMessage();
       logger.log(Level.ERROR,
