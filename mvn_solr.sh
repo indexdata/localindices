8f57756b13ed326a45c5fac4b2286fb1d5ec3467
Dennis Schafroth <dennis@indexdata.com>
2013-01-28 14:50:13 +0100

    Change to use profile. Correct PID handling. Give time to solr/jetty to start.

--- mvn_solr.sh
+++ mvn_solr.sh
@@ -1,6 +1,8 @@
 
 # Start up Solr in the jetty container
-java -jar start.jar & 
-PID=$$
-mvn $*
+java -jar start.jar > solr.log 2>&1 &
+PID=$!
+echo "Solr starting ($PID)" 
+sleep 10
+mvn -P integration-tests $*
 kill $PID
