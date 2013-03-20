1a86304b86c112fc8fc052b69712d891367b7916
Dennis Schafroth <dennis@indexdata.com>
2013-03-20 02:37:06 +0100

    Add AsyncMessageConsumer interface

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/messaging/MessageRouter.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/messaging/MessageRouter.java
@@ -1,7 +1,7 @@
 package com.indexdata.masterkey.localindices.harvest.messaging;
 
 
-public interface MessageRouter<T> extends Runnable {
+public interface MessageRouter<T> extends Runnable, AsyncMessageConsumer<T>  {
   void setInput(MessageConsumer<T> input);
   void setOutput(MessageProducer<T> output);
   void setError(MessageProducer<T> error);
