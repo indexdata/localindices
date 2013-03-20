8d2b8c4ac9c5169608234269e1a4097e83d0d11d
Dennis Schafroth <dennis@indexdata.com>
2013-03-20 02:40:51 +0100

    Use Router factory

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/ThreadedTransformationRecordStorageProxy.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/ThreadedTransformationRecordStorageProxy.java
@@ -7,13 +7,12 @@ import java.util.Map;
 import javax.xml.transform.TransformerConfigurationException;
 
 import com.indexdata.masterkey.localindices.entity.TransformationStep;
-import com.indexdata.masterkey.localindices.entity.XmlTransformationStep;
 import com.indexdata.masterkey.localindices.harvest.job.StorageJobLogger;
 import com.indexdata.masterkey.localindices.harvest.messaging.BlockingMessageQueue;
 import com.indexdata.masterkey.localindices.harvest.messaging.MessageQueue;
 import com.indexdata.masterkey.localindices.harvest.messaging.MessageRouter;
+import com.indexdata.masterkey.localindices.harvest.messaging.RouterFactory;
 import com.indexdata.masterkey.localindices.harvest.messaging.StopMessage;
-import com.indexdata.masterkey.localindices.harvest.messaging.XmlTransformerRouter;
 
 public class ThreadedTransformationRecordStorageProxy extends RecordStorageProxy {
   private StorageJobLogger logger;
@@ -23,7 +22,8 @@ public class ThreadedTransformationRecordStorageProxy extends RecordStorageProxy
   private MessageQueue<Object> error = new BlockingMessageQueue<Object>();
   private Thread lastThread = null;
   private List<TransformationStep> steps;
-  private String databaseID; 
+  private String databaseID;
+  private MessageRouter<Object>[] messageRouters; 
   
   
   public ThreadedTransformationRecordStorageProxy(RecordStorage storage, 
@@ -40,19 +40,19 @@ public class ThreadedTransformationRecordStorageProxy extends RecordStorageProxy
   {
     MessageQueue<Object> current = source; 
     MessageRouter router = null;
-    int index = 0;
+    RouterFactory factory = RouterFactory.newInstance();
     if (steps != null) {
+      int index = 0;
+      messageRouters = new MessageRouter[steps.size()];
       for (TransformationStep step: steps) {
-        if (step instanceof XmlTransformationStep) {
-          XmlTransformerRouter xmlRouter = new XmlTransformerRouter(step);
-          router = xmlRouter;
-        }
+	router = factory.create(step);
+	messageRouters[index++] = router;
         router.setError(error);
         router.setInput(current);
         current = new BlockingMessageQueue();
         router.setOutput(current);
         Thread thread = new Thread(router);
-        thread.setName("XmlTransformerRouter " + index++); 
+        thread.setName("Router(" + step.getName() + ")"); 
         thread.start();
         lastThread = thread;
       }
