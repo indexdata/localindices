e011c5ac7e81b57c93dd51b56874f3e3c24309aa
Dennis Schafroth <dennis@indexdata.com>
2013-03-20 16:58:18 +0100

    Move class out

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/TransformationChainRecordStorageProxy.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/TransformationChainRecordStorageProxy.java
@@ -5,66 +5,28 @@ import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.sax.SAXResult;
-import javax.xml.transform.sax.SAXSource;
-import javax.xml.transform.sax.SAXTransformerFactory;
 
-//import org.apache.commons.io.input.TeeInputStream;
-import org.xml.sax.ContentHandler;
-import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 
+import com.indexdata.masterkey.localindices.harvest.job.RecordHarvestJob;
 import com.indexdata.masterkey.localindices.harvest.job.StorageJobLogger;
-import com.indexdata.xml.factory.XmlFactory;
+//import org.apache.commons.io.input.TeeInputStream;
 
 public class TransformationChainRecordStorageProxy extends RecordStorageProxy {
   private PipedOutputStream output;
   private PipedInputStream input;
   private Thread thread = null;
-  private SAXTransformerFactory stf = (SAXTransformerFactory) XmlFactory.newTransformerInstance();
-  private Transformer transformer;
-  private TransformerException transformException = null;
-  private StorageJobLogger logger; 
+  private StorageJobLogger logger;
   
-  public TransformationChainRecordStorageProxy(final RecordStorage storage,
-      final XMLReader xmlFilteredReader, final ContentHandler storageHandler, final StorageJobLogger logger) throws IOException,
+  public TransformationChainRecordStorageProxy(RecordStorage storage, XMLReader xmlFilteredReader, 
+      RecordHarvestJob job) throws IOException,
       TransformerConfigurationException {
-    // this.xmlFilter = xmlFilter;
-    this.logger = logger;
+    this.logger = job.getLogger();
     setTarget(storage);
     input = new PipedInputStream();
     output = new PipedOutputStream(input);
-    transformer = stf.newTransformer();
-    // this.storageHandler = storageHandler;
-    thread = new Thread(new Runnable() {
-      public void run() {
-	processDataFromInputStream(input);
-      };
-
-      private void processDataFromInputStream(PipedInputStream input) {
-	InputSource source = new InputSource(input);
-	SAXSource transformSource = new SAXSource(xmlFilteredReader, source);
-	SAXResult result = new SAXResult(storageHandler);
-	try {
-	  transformer.transform(transformSource, result);
-	} catch (TransformerException e) {
-	  if (logger != null) 
-	    logger.error("Transformation exception. Save for later ", e);
-	  e.printStackTrace();
-	  transformException = e;
-	  try { 
-	    output.close();
-	  }
-	  catch (IOException ioe) 
-	  {
-	    logger.error("IOException while closing output");
-	  }
-	}
-      };
-    });
+    thread = new Thread(new TransformPipedInputStream(input, output, storage.getContentHandler(), job));
     thread.start();
   }
 
@@ -87,11 +49,13 @@ public class TransformationChainRecordStorageProxy extends RecordStorageProxy {
 	logger.error("Interrupted before joined", e);
       e.printStackTrace();
     }
+/*
     if (transformException != null) {
       if (logger != null) 
 	logger.error("Throw saved Transformation exception as IOException", transformException);
       throw new IOException("Transformation failed", transformException);
     }
+*/
     super.commit();
   }
 
