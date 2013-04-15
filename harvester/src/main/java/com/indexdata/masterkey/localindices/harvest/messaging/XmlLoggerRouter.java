344415c3bab78ec63036d7537419672f5d6da664
Dennis Schafroth <dennis@indexdata.com>
2013-04-15 12:16:09 +0200

    Logger for XML

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/messaging/XmlLoggerRouter.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/messaging/XmlLoggerRouter.java
@@ -3,7 +3,10 @@ package com.indexdata.masterkey.localindices.harvest.messaging;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
@@ -12,6 +15,12 @@ import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.sax.SAXTransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.PatternLayout;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
 
@@ -35,7 +44,7 @@ public class XmlLoggerRouter implements MessageRouter {
   private MessageProducer error;
   private Transformer transformer;
   private String charname = "UTF-8";
-  
+  private Thread workerThread = null;
   private boolean running = true;
   HarvestJob job;
   StorageJobLogger logger;
@@ -46,11 +55,7 @@ public class XmlLoggerRouter implements MessageRouter {
     logger = job.getLogger();
     if (step instanceof CustomTransformationStep) {
       this.step = (CustomTransformationStep) step;
-      try {
-	transformer = stf.newTransformer();
-      } catch (TransformerConfigurationException e) {
-	e.printStackTrace();
-      }
+      setup(this.step);
     }
     else throw new RuntimeException("Configuration Error: Not a XmlTransformationStep");
   }
@@ -58,10 +63,74 @@ public class XmlLoggerRouter implements MessageRouter {
   public XmlLoggerRouter(CustomTransformationStep step, RecordHarvestJob job) {
     this.step = step;
     logger = job.getLogger();
+    setup(step);
+  }
+
+  @SuppressWarnings("unchecked")
+  private void setup(CustomTransformationStep step) {
     try {
+      Map<String, Object> map = new HashMap<String, Object>();   
+      if (step.getScript() != null) {
+        logger.debug("Logger configuration: " + step.getScript());
+        JSONParser parser = new JSONParser();
+        Object config = parser.parse(step.getScript());
+        if (config instanceof Map)
+          map = (Map) config;
+        else 
+          logger.debug("Not an Map object?");
 	transformer = stf.newTransformer();
+      }
+      else 
+        logger.debug("Using Job Logger for XML logging. " + logger.getIdentify());
+	
+      if (map.containsKey("indent")) {
+	logger.log(Level.DEBUG, "Configuring XML indent " + map.get("indent").toString() + "");
+	transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", map.get("indent").toString());
+      }
+      Appender appender = null;
+      if (map.containsKey("appender")) {
+	String appenderString = map.get("appender").toString(); 
+	Class<? extends Appender> appenderClass = (Class<? extends Appender>) Class.forName(appenderString);
+	appender = appenderClass.newInstance();
+	logger.log(Level.DEBUG, "Appender (" + appenderString + ")");
+      }
+      if (map.containsKey("file")) {
+	FileAppender fileAppender = new FileAppender();
+	fileAppender.setFile(map.get("file").toString());
+	if (map.get("append") != null) 
+	  fileAppender.setAppend("true".equals(map.get("append")));
+	logger.log(Level.DEBUG, "FileAppender (" + fileAppender.getFile() + ") appending " + fileAppender.getAppend());
+	appender = fileAppender;
+      }
+      if (map.get("layout") != null) {
+	String pattern = map.get("layout").toString();
+	PatternLayout layout = new PatternLayout(pattern);
+	logger.log(Level.DEBUG, "Configure appender with pattern Layout: " + pattern + "");
+	if (appender != null) 
+	  appender.setLayout(layout);
+      }
+      if (appender != null) {
+	appender.setName(step.getName());
+	logger.log(Level.DEBUG, "Add Appender of class " + appender.getClass());
+	logger.addAppender(appender);
+      }
+      
     } catch (TransformerConfigurationException e) {
-	e.printStackTrace();
+      logger.log(Level.ERROR, "Unable to create XML Transformer", e);
+      e.printStackTrace();
+    } catch (ParseException e) {
+      logger.log(Level.ERROR, "Unable to parse JSON configuration: " + step.getScript(), e);
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+	logger.error("Class not found: " + e.getMessage(), e);
+      e.printStackTrace();
+    } catch (InstantiationException e) {
+	logger.error("Unanble to instantioate class: " + e.getMessage(), e);
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
   }
 
@@ -161,6 +230,8 @@ public class XmlLoggerRouter implements MessageRouter {
   @Override
   public void shutdown() {
     running = false;
+    if (workerThread != null)
+      workerThread.interrupt();
   }
 
   public void setXmlTransformer(Transformer transformer) {
@@ -176,6 +247,11 @@ public class XmlLoggerRouter implements MessageRouter {
   public Object take() throws InterruptedException {
     return input.take();
     //return null;
+  }
+
+  @Override
+  public void setThread(Thread thread) {
+    workerThread = thread;
   }
 
 }
