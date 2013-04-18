8fdbee3081809452d6b994b410bcb8d07890458c
Dennis Schafroth <dennis@indexdata.com>
2013-04-18 13:01:39 +0200

    new Custom class step

--- sql/add_2.2.sql
+++ sql/add_2.2.sql
@@ -5,5 +5,5 @@ alter table HARVESTABLE add column SCRIPT   varchar(102400);
 alter table HARVESTABLE add column ISPERSISTENCE boolean;
 alter table HARVESTABLE add column INITDATA varchar(4096);
 alter table HARVESTABLE add column SLEEP    integer;
-alter table TRANSFORMATION add column `PARALLEL` tinyint(1) DEFAULT NULL,
-
+alter table TRANSFORMATION add column `PARALLEL` tinyint(1) DEFAULT NULL;
+alter table STEP       add column `CUSTOMCLASS` varchar(255) DEFAULT NULL;
