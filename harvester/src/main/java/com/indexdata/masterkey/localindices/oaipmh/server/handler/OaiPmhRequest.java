7b8e619d92bc5ec4eb44c00aab8680a7f2057978
Dennis Schafroth <dennis@indexdata.com>
2013-05-16 16:01:14 +0200

    Interface to wrap HttpServletRequest

---
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/handler/OaiPmhRequest.java
@@ -0,0 +1,9 @@
+package com.indexdata.masterkey.localindices.oaipmh.server.handler;
+
+public interface OaiPmhRequest {
+  
+  String getParameter(String parameter);
+  String getParameterValue(String parameter);
+  String getUrl();
+
+}
