27e5915d8b26acf68b2c8d9daa8f097cd59f445f
Dennis Schafroth <dennis@indexdata.com>
2013-05-17 15:00:50 +0200

    Dynamic loading of classes and instances

--- harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/handler/implement/mockup/MockUpDispatcher.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/handler/implement/mockup/MockUpDispatcher.java
@@ -5,25 +5,34 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
+
 import com.indexdata.masterkey.localindices.oaipmh.server.Dispatcher;
 import com.indexdata.masterkey.localindices.oaipmh.server.handler.OaiPmhHandler;
 
 public class MockUpDispatcher implements Dispatcher {
 
-  Map<String, OaiPmhHandler> dispatchMap = new HashMap<String, OaiPmhHandler>(); 
+  Map<String, String> dispatchMap = new HashMap<String, String>(); 
+  String packageName = "com.indexdata.masterkey.localindices.oaipmh.server.handler.implement.mockup"; 
 
-  // TODO problem with this approach is that every request gets same Handler
   public MockUpDispatcher() {
-    dispatchMap.put("ListRecords", new ListRecords());
+    dispatchMap.put("ListRecords", "ListRecords");
   }
 
+  @SuppressWarnings("unchecked")
   @Override
-  public OaiPmhHandler onRequest(HttpServletRequest req) {
+  public OaiPmhHandler onRequest(HttpServletRequest req) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
     
     String verb = req.getParameter("verb");
     if (verb == null) 
       	throw new RuntimeException("Missing parameter verb");
-    OaiPmhHandler handler = dispatchMap.get(verb);
+    String handlerClassName = dispatchMap.get(verb);
+    if (handlerClassName == null) {
+      Logger.getLogger(this.getClass()).warn("Attempting using '" + verb + "' as class name");
+      handlerClassName = verb;
+    }
+    Class<? extends OaiPmhHandler> handlerClass = (Class<? extends OaiPmhHandler>) Class.forName(packageName + "." + handlerClassName);
+    OaiPmhHandler handler = handlerClass.newInstance();
     if (handler == null)
       throw new RuntimeException("No Handler implemented for verb '" + verb + "'");
     return handler;
