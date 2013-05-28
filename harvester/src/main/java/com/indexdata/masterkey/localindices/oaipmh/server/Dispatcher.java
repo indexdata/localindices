88efa0ae8efcadf72606b7f64d586a55fb898fa4
Dennis Schafroth <dennis@indexdata.com>
2013-05-28 13:38:54 +0200

    Throw OaiPmh protocol exception instead

--- harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/Dispatcher.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/Dispatcher.java
@@ -2,11 +2,12 @@ package com.indexdata.masterkey.localindices.oaipmh.server;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.indexdata.masterkey.localindices.oaipmh.server.handler.OaiPmhProcotolException;
 import com.indexdata.masterkey.localindices.oaipmh.server.handler.OaiPmhHandler;
 
 public interface Dispatcher {
   
   
-  OaiPmhHandler onRequest(HttpServletRequest req) throws ClassNotFoundException, InstantiationException, IllegalAccessException;
+  OaiPmhHandler onRequest(HttpServletRequest req) throws OaiPmhProcotolException;
 
 }
