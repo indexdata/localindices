4567c055a4d23b02f7942e170e1451785d403b64
Dennis Schafroth <dennis@indexdata.com>
2013-05-28 13:38:11 +0200

    Throw OaiPmh exception

--- harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/handler/OaiPmhHandler.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/handler/OaiPmhHandler.java
@@ -3,7 +3,7 @@ package com.indexdata.masterkey.localindices.oaipmh.server.handler;
 
 public interface OaiPmhHandler {
   
-  OaiPmhResponse handle(OaiPmhRequest request);
+  OaiPmhResponse handle(OaiPmhRequest request) throws OaiPmhProcotolException;
   public void verifyParameters(OaiPmhRequest request, String[][] parameters);
   
 
