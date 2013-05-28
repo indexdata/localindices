1be82d58f9d215765e3d7245e36d5a60733c6b33
Dennis Schafroth <dennis@schafroth.com>
2013-05-28 17:32:09 +0200

    Moved to OaiPmh Response

--- harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/handler/implement/mockup/CommonOaiPmhHandler.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/handler/implement/mockup/CommonOaiPmhHandler.java
@@ -1,32 +1,12 @@
 package com.indexdata.masterkey.localindices.oaipmh.server.handler.implement.mockup;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
-
 import com.indexdata.masterkey.localindices.oaipmh.server.handler.OaiPmhHandler;
 import com.indexdata.masterkey.localindices.oaipmh.server.handler.OaiPmhRequest;
 
 public abstract class CommonOaiPmhHandler implements OaiPmhHandler {
 
-  static SimpleDateFormat requestDateFormater = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-  static {
-    requestDateFormater.setTimeZone(TimeZone.getTimeZone("UTC"));
-  }
-  protected String oaiPmhHeader = "<OAI-PMH xmlns=\"http://www.openarchives.org/OAI/2.0/\" xmlns:oai=\"http://www.openarchives.org/OAI/2.0/\"\n" + 
-        "	xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:marc=\"http://www.loc.gov/MARC21/slim\"\n" + 
-        "	xsi:schemaLocation=\"http://www.openarchives.org/OAI/2.0/\n" + 
-        "          http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd\">\n";
-  protected String oaiPmhEnd = "</OAI-PMH>\n";
-
   public CommonOaiPmhHandler() {
     
-  }
-
-  public String getResponseDate() {     
-    String responseDate = 
-              "	<responseDate>" + requestDateFormater.format(new Date()) + "</responseDate>\n";
-    return responseDate;
   }
 
   public void verifyParameters(OaiPmhRequest request, String[][] parameters) {
