6b83a7a2a54039bbf959d8899e51c97b935e9570
Jakub Skoczen <jakub@indexdata.dk>
2013-06-20 20:42:49 +0200

    Use animated gif for the progress indicator

--- harvester-admin/src/main/webapp/resources/ajaxloader/ajaxloader.js
+++ harvester-admin/src/main/webapp/resources/ajaxloader/ajaxloader.js
@@ -6,15 +6,28 @@ if (!window["busystatus"]) {
   var busystatus = {};
 }
 
+if (!window.busystatus.getProgressBox) {
+  busystatus.getProgressBox = function () {
+    var div = document.getElementById('progress-meter');
+    if (div === null) {
+      div = document.createElement('div');
+      div.setAttribute('id', 'progress-meter');
+      div.style.display = 'none';
+      document.getElementsByTagName('body')[0].appendChild(div);
+    }
+    return div;
+  };
+}
+
 if (!window.busystatus.onStatusChange) {
   busystatus.onStatusChange = function onStatusChange(data) {
     //ignore poll evenens
     if (data.source.id.indexOf("poll", this.length - "poll".length) !== -1) return;
     var status = data.status;
     if (status === "begin") { // turn on busy indicator
-      document.body.style.cursor = 'wait';
+      busystatus.getProgressBox().style.display = 'block';
     } else { // turn off busy indicator, on either "complete" or "success"
-      document.body.style.cursor = 'auto';
+      busystatus.getProgressBox().style.display = 'none';
     }
   };
 }
