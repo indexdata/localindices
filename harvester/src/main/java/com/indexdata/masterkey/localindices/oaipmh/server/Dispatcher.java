53fec4845b171c833f7160448aed273a20609126
Dennis Schafroth <dennis@indexdata.com>
2013-07-30 16:33:51 +0200

    Make interface independent of Servlet API

--- harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/Dispatcher.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/Dispatcher.java
@@ -1,13 +1,12 @@
 package com.indexdata.masterkey.localindices.oaipmh.server;
 
-import javax.servlet.http.HttpServletRequest;
-
-import com.indexdata.masterkey.localindices.oaipmh.server.handler.OaiPmhProcotolException;
+ import com.indexdata.masterkey.localindices.oaipmh.server.handler.OaiPmhProcotolException;
 import com.indexdata.masterkey.localindices.oaipmh.server.handler.OaiPmhHandler;
+import com.indexdata.masterkey.localindices.oaipmh.server.handler.OaiPmhRequest;
 
 public interface Dispatcher {
   
   
-  OaiPmhHandler onRequest(HttpServletRequest req) throws OaiPmhProcotolException;
+  OaiPmhHandler onRequest(OaiPmhRequest req) throws OaiPmhProcotolException;
 
 }
