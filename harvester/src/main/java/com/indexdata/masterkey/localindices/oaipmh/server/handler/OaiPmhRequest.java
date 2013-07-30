823510f560712824cef6aa17658fd55a1354e44e
Dennis Schafroth <dennis@indexdata.com>
2013-07-30 16:36:00 +0200

    Extend with HttpServletRequest-like getParameterValues method

--- harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/handler/OaiPmhRequest.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/handler/OaiPmhRequest.java
@@ -4,6 +4,7 @@ public interface OaiPmhRequest {
   
   String getParameter(String parameter);
   String getParameterValue(String parameter);
+  String[] getParameterValues(String parameter);
   String getBaseUrl();
 
 }
