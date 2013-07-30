59387aaec24a569589f4578fa8568b6ec6e0a8c4
Dennis Schafroth <dennis@indexdata.com>
2013-07-30 16:35:16 +0200

    Consider other API

--- harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/handler/OaiPmhHandler.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/handler/OaiPmhHandler.java
@@ -2,9 +2,9 @@ package com.indexdata.masterkey.localindices.oaipmh.server.handler;
 
 
 public interface OaiPmhHandler {
-  
-  OaiPmhResponse handle(OaiPmhRequest request) throws OaiPmhProcotolException;
+
+  OaiPmhResponse handle(OaiPmhRequest request) throws OaiPmhProcotolException, OaiPmhServerException;
+  //void handle(OaiPmhRequest request, OaiPmhResponse response) throws OaiPmhProcotolException;
   public void verifyParameters(OaiPmhRequest request, String[][] parameters);
   
-
 }
