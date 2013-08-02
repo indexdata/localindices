778eb0c1ea00f0add16bdca29b64f303c2ea98dc
Dennis Schafroth <dennis@indexdata.com>
2013-08-02 12:46:56 +0200

    Comment out the new methods

--- harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/handler/OaiPmhRequest.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/handler/OaiPmhRequest.java
@@ -1,11 +1,12 @@
 package com.indexdata.masterkey.localindices.oaipmh.server.handler;
 
 public interface OaiPmhRequest {
+/*
   String getVerb(); 
   String getSet() throws OaiPmhProcotolException;
   String getStartDate();
   String getUntilDate();
-  
+*/
   String getParameter(String parameter);
   String getParameterValue(String parameter);
   String[] getParameterValues(String parameter);
