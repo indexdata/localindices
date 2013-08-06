b55143aa9da769a7e48762402d8c23238e856c19
Dennis Schafroth <dennis@schafroth.com>
2013-08-06 19:22:56 +0200

    Added progress counter. Not sure it's a good idea

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/job/RecordStorageConsumer.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/job/RecordStorageConsumer.java
@@ -12,15 +12,20 @@ import com.indexdata.xml.filter.MessageConsumer;
 public class RecordStorageConsumer implements MessageConsumer {
 
   RecordStorage recordStorage; 
-  
-  public RecordStorageConsumer(RecordStorage storage) {
+  StorageJobLogger logger;
+  long added = 0; 
+  public RecordStorageConsumer(RecordStorage storage, StorageJobLogger logger) {
     recordStorage = storage;
+    this.logger = logger;
+    
   }
   
   @Override
   public void accept(Node xmlNode) {
     Record record = new RecordDOMImpl(null, null, xmlNode);
     recordStorage.add(record);
+    if (++added % 1000 == 0)
+      	logger.info("Added " + added + " records");
   }
 
   @Override
