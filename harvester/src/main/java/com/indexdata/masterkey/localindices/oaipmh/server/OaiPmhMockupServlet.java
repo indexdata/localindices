c3aa6551453eb61f24ad3f5d42f019f3a22f7247
Dennis Schafroth <dennis@schafroth.com>
2013-08-06 19:20:04 +0200

    Fix warning: optimize imports.

--- harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/OaiPmhMockupServlet.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/oaipmh/server/OaiPmhMockupServlet.java
@@ -7,11 +7,9 @@
 package com.indexdata.masterkey.localindices.oaipmh.server;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
