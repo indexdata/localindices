69bf8ad34caa450070a37a244acf33040f96afac
Jakub Skoczen <jakub@indexdata.dk>
2013-08-15 17:01:40 +0200

    More notes on charsets

--- sql/conver_to_unicode.sql
+++ sql/conver_to_unicode.sql
@@ -1,8 +1,10 @@
 -- there are problems with storing utf8 strings in the table
 -- in order to convert the table we need to modify fields
 -- check charset with: show full columns from HARVESTABLE;
--- to set the default for new tables use:
+-- Note: to set the default for new tables use:
 -- ALTER DATABASE localindices CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- Note2: it is recomended to use utf8_unicode_ci instead utf8_general_ci
+-- whic is more precise
 
 ALTER TABLE HARVESTABLE MODIFY DESCRIPTION TEXT;
 ALTER TABLE HARVESTABLE MODIFY TECHNICALNOTES TEXT;
