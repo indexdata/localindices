990cf6f525a7b9b04d8933e47755c64ccbf031b1
Dennis Schafroth <dennis@indexdata.com>
2013-08-16 11:48:38 +0200

    Remove double add count

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/job/AbstractRecordHarvestJob.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/job/AbstractRecordHarvestJob.java
@@ -135,7 +135,7 @@ public abstract class AbstractRecordHarvestJob extends AbstractHarvestJob implem
       StorageStatus storageStatus = storage.getStatus();  
       if (storageStatus != null) {
         resource.setAmountHarvested(storageStatus.getAdds());
-        logger.info("Committed " + storageStatus.getAdds() 
+        logger.info("Committed "  
             	     + (storageStatus.getAdds() > 0 ?  storageStatus.getAdds() + " adds " : "")  
             	     + (storageStatus.getDeletes() > 0 ?  storageStatus.getDeletes() + " deletes" : "") 
             	     + storageStatus.getTotalRecords() + " in total (pending warming of index).");
