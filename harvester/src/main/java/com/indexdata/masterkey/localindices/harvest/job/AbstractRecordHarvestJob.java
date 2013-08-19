2c8690369edf1496e2be227ef5050aa062a05447
Dennis Schafroth <dennis@indexdata.com>
2013-08-19 16:23:35 +0200

    And a space too much

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/job/AbstractRecordHarvestJob.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/job/AbstractRecordHarvestJob.java
@@ -137,7 +137,7 @@ public abstract class AbstractRecordHarvestJob extends AbstractHarvestJob implem
       if (storageStatus != null) {
         resource.setAmountHarvested(storageStatus.getAdds());
         logger.info("Committed "  
-            	     + (storageStatus.getAdds() > 0 ?  storageStatus.getAdds() + " adds " : " ")  
+            	     + (storageStatus.getAdds() > 0 ?  storageStatus.getAdds() + " adds " : "")  
             	     + (storageStatus.getDeletes() > 0 ?  storageStatus.getDeletes() + " deletes " : "") 
             	     + storageStatus.getTotalRecords() + " in total (pending warming of index).");
       }
