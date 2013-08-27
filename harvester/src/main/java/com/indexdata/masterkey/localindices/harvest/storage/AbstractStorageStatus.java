b9e6d09092704ad190ce54c9b0a07ca4b1112cbe
Dennis Schafroth <dennis@indexdata.com>
2013-08-27 16:01:51 +0200

    Pretty printing of status for testing

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/AbstractStorageStatus.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/AbstractStorageStatus.java
@@ -6,7 +6,7 @@ package com.indexdata.masterkey.localindices.harvest.storage;
 
 /**
  *
- * @author dennis
+ * @author Dennis
  */
 public abstract class AbstractStorageStatus implements StorageStatus {
 
@@ -16,5 +16,10 @@ public abstract class AbstractStorageStatus implements StorageStatus {
 	   status.getDeletes().equals(getDeletes()) && 
 	   status.getTotalRecords().equals(getTotalRecords());
   }
-  
+
+  public String toString() {
+    StringBuffer result = new StringBuffer("Adds: ");
+    result.append(getAdds()).append(" Deletes: ").append(getDeletes()).append(" Total: ").append(getTotalRecords());
+    return result.toString();
+  }
 }
