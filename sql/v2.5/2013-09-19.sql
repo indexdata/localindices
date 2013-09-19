434f107921899bb3fb9f1c6e2cac2e35c163083d
Dennis Schafroth <dennis@schafroth.com>
2013-09-19 23:13:41 +0200

    New fields for mail and log level

---
+++ sql/v2.5/2013-09-19.sql
@@ -0,0 +1,6 @@
+alter table STORAGE add column CUSTOMCLASS varchar(1024);
+alter table STORAGE add column LOGLEVEL varchar(30);
+alter table STORAGE add column MAILLEVEL varchar(30);
+alter table HARVESTABLE add column LOGLEVEL varchar(30);
+alter table HARVESTABLE add column MAILLEVEL varchar(30);
+
