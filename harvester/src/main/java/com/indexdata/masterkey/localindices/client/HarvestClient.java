ce1a1d7ac26c32403dfb8c01d29c534d548cd47b
Dennis Schafroth <dennis@indexdata.com>
2013-10-16 16:03:31 +0200

    Add common connection creator method

--- harvester/src/main/java/com/indexdata/masterkey/localindices/client/HarvestClient.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/client/HarvestClient.java
@@ -1,8 +1,12 @@
 package com.indexdata.masterkey.localindices.client;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public interface HarvestClient {
   
   int download(URL url) throws Exception;
+  HttpURLConnection createConnection(URL url) throws IOException;
 }
+
