ecfcb625ca24e2952e677666dbd76f065c555b6b
Jakub Skoczen <jakub@indexdata.dk>
2013-11-06 16:24:38 +0100

    Set up default engine/repo on schema update

--- sql/v2.5/2013-11-04.sql
+++ sql/v2.5/2013-11-04.sql
@@ -6,6 +6,14 @@ CREATE TABLE `SETTING` (
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `SETTING` VALUES (1, "cf.engine.url.1", "CF Engine (default)", "http://connect.indexdata.com:9010/connector");
+
+INSERT INTO `SETTING` VALUES (2, "cf.repo.url.1", "CF Repo (default)", "https://idtest:idtest36@cfrepo.indexdata.com/repo.pl/idtest/");
+
 alter table `HARVESTABLE` add column `CONNECTORENGINEURLSETTING_ID` bigint(20) DEFAULT NULL;
+
+UPDATE `HARVESTABLE` SET `CONNECTORENGINEURLSETTING_ID`=1;
+
 alter table `HARVESTABLE` add column `CONNECTORREPOURLSETTING_ID` bigint(20) DEFAULT NULL;
 
+UPDATE `HARVESTABLE` SET `CONNECTORREPOURLSETTING_ID`=2;
