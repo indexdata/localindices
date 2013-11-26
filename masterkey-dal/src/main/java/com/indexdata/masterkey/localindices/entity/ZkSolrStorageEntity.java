231dc95c061d83fc02d4708d930b161c046c7e78
Dennis Schafroth <dennis@indexdata.com>
2013-11-26 15:47:38 +0100

    ZooKeeper entity

---
+++ masterkey-dal/src/main/java/com/indexdata/masterkey/localindices/entity/ZkSolrStorageEntity.java
@@ -0,0 +1,23 @@
+package com.indexdata.masterkey.localindices.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement(name = "solrStorage")
+public class ZkSolrStorageEntity extends Storage implements Serializable {
+
+  private static final long serialVersionUID = -5840585258242340150L;
+
+  public String getSearchUrl(Harvestable resource) {
+    if (resource == null)
+      	return getSearchUrl();
+    StringBuffer clientUrl = new  StringBuffer(getSearchUrl());
+    if (clientUrl.lastIndexOf("/") + 1 != clientUrl.length())
+      clientUrl.append('/');
+    clientUrl.append("select?q=database:").append(resource.getId());
+    return clientUrl.toString();
+  }
+}
