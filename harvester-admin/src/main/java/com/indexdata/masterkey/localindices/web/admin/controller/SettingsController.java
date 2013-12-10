f34c4e7bc5c83b2a42ada7ee88707ab3b64168b6
Jakub Skoczen <jakub@indexdata.dk>
2013-12-10 13:01:44 +0100

    Add Setting CRUD UI

--- harvester-admin/src/main/java/com/indexdata/masterkey/localindices/web/admin/controller/SettingsController.java
+++ harvester-admin/src/main/java/com/indexdata/masterkey/localindices/web/admin/controller/SettingsController.java
@@ -6,12 +6,14 @@
 package com.indexdata.masterkey.localindices.web.admin.controller;
 
 import com.indexdata.masterkey.localindices.dao.DAOException;
+import com.indexdata.masterkey.localindices.dao.EntityInUse;
 import com.indexdata.masterkey.localindices.dao.SettingDAO;
 import com.indexdata.masterkey.localindices.dao.SettingDAOFactory;
 import com.indexdata.masterkey.localindices.entity.Setting;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
 import org.apache.log4j.Level;
@@ -26,6 +28,8 @@ import org.apache.log4j.Logger;
 public class SettingsController {
   Logger logger = Logger.getLogger("com.indexdata.masterkey.localindices.admin");
   SettingDAO dao;
+  Setting setting = new Setting();
+  private List<Setting> settings;
 
   public SettingsController() {
     try {
@@ -36,6 +40,11 @@ public class SettingsController {
     }
   }
   
+  public void initialize() {
+    logger.info("Retrieving settings from the harvester...");
+    settings = dao.retrieve(0, 100);
+  }
+  
   /**
    * Lists available connector engine URLs.
    * @return 
@@ -44,12 +53,51 @@ public class SettingsController {
     return dao.retrieve(0, 100, "cf.engine.url.", false);
   }
   
+  public List<Setting> getSettings() {
+    return settings;
+  }
+  
+  
     /**
    * Lists available connector repo URLs.
    * @return 
    */
   public List<Setting> getConnectorRepos() {
     return dao.retrieve(0, 100, "cf.repo.url.", false);
+  }
+
+  public Setting getSetting() {
+    return setting;
+  }
+
+  public void setSetting(Setting setting) {
+    this.setting = setting;
+  }
+  
+  //ui methods
+  
+  public String save(Setting s) {
+    dao.update(s);
+    return null; //same page
+  }
+  
+  public String add() {
+    dao.create(setting);
+    setting = new Setting();
+    return null;
+  }
+  
+  public String delete(Setting s) {
+    try {
+      dao.delete(s);
+    } catch (EntityInUse eiu) {
+      return "failure";
+    }
+    return null;
+  }
+  
+  public String list() {
+    return "settings";
   }
   
 }
