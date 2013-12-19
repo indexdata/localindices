f8338530a1166fc5fa5d533d921a7e826ee60618
Dennis Schafroth <dennis@indexdata.com>
2013-12-19 13:32:00 +0100

    Create based on Storage. Moved the setHarvestable up

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/HarvestStorageFactory.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/HarvestStorageFactory.java
@@ -23,16 +23,14 @@ public class HarvestStorageFactory {
    * @param harvestable
    * @return
    */
-  public static RecordStorage getStorage(Harvestable harvestable) {
+  public static RecordStorage getStorage(Storage entity) {
     RecordStorage harvestStorage = null;
-    Storage entity = (Storage) harvestable.getStorage();
     /* TODO Extend to create other types */
     if (entity.getCustomClass() != null && !"".equals(entity.getCustomClass())) {
 	try {
 	  Class<?> storage = Class.forName(entity.getCustomClass());
 	  Object object = storage.newInstance();
 	  RecordStorage recordStorage = (RecordStorage) object;
-	  recordStorage.setHarvestable(harvestable);
 	  return recordStorage;
 	} catch (ClassNotFoundException e) {
 	  e.printStackTrace();
@@ -47,17 +45,14 @@ public class HarvestStorageFactory {
     }
     else if (entity instanceof com.indexdata.masterkey.localindices.entity.ZkSolrStorageEntity) {
       SolrRecordStorage storage = new ZooKeeperSolrRecordStorage();
-      storage.setHarvestable(harvestable);
       return storage;
     }
     else if (entity instanceof com.indexdata.masterkey.localindices.entity.SolrStorageEntity) {
       SolrRecordStorage storage = new BulkSolrRecordStorage();
-      storage.setHarvestable(harvestable);
       return storage;
     }
     else if (entity instanceof FileStorageEntity) {
       FileStorage storage = new FileStorage();
-      storage.setHarvestable(harvestable);
       return storage;
     }
     else {
