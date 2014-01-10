3ed550bf1e9c648d6f95674fe043209357e59f09
Dennis Schafroth <dennis@indexdata.com>
2014-01-10 14:33:11 +0100

    New scheduler testing under construction

---
+++ harvester/src/test/java/com/indexdata/masterkey/localindices/scheduler/TestJobScheduler.java
@@ -0,0 +1,45 @@
+package com.indexdata.masterkey.localindices.scheduler;
+
+import java.util.HashMap;
+import java.util.Properties;
+
+import com.indexdata.masterkey.localindices.dao.HarvestableDAO;
+import com.indexdata.masterkey.localindices.dao.StorageDAO;
+import com.indexdata.masterkey.localindices.dao.bean.HarvestableDAOFake;
+import com.indexdata.masterkey.localindices.dao.bean.StorageDAOFake;
+import com.indexdata.masterkey.localindices.entity.Harvestable;
+import com.indexdata.masterkey.localindices.entity.XmlBulkResource;
+import com.indexdata.masterkey.localindices.harvest.job.JobTester;
+
+import junit.framework.TestCase;
+
+public class TestJobScheduler extends JobTester {
+  
+  private JobScheduler jobScheduler; 
+  private HarvestableDAO harvestableDao = new HarvestableDAOFake();
+  private StorageDAO  storageDao = new StorageDAOFake();
+
+  void testJobScheduler() {
+    SchedulerThread schedulerThread = new SchedulerThread(new HashMap<String, Object>(), new Properties());  
+    jobScheduler = schedulerThread.getScheduler();
+    jobScheduler.setHarvestableDao(harvestableDao);
+    jobScheduler.setStorageDao(storageDao);
+    
+    Thread thread = new Thread(schedulerThread);
+    thread.start(); 
+    Harvestable entity = new XmlBulkResource("http://localhost:8080/harvester/oaipmh");
+    harvestableDao.create(entity);
+    harvestableDao.command(entity, "start");
+    harvestableDao.create(entity);
+    
+    
+    schedulerThread.kill();
+    try {
+      thread.join();
+    } catch (InterruptedException ie) {
+      
+    }
+    
+  }
+
+}
