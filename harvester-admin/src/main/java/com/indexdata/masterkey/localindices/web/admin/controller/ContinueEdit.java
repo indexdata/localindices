3ab8262a2e2865a773e4e241da0f74649c5b05e9
Dennis Schafroth <dennis@indexdata.com>
2014-03-14 10:27:46 +0100

    Exception to indicate we need to continue editing due to error

---
+++ harvester-admin/src/main/java/com/indexdata/masterkey/localindices/web/admin/controller/ContinueEdit.java
@@ -0,0 +1,10 @@
+package com.indexdata.masterkey.localindices.web.admin.controller;
+
+public class ContinueEdit extends Exception {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -3430731725193581797L;
+
+}
