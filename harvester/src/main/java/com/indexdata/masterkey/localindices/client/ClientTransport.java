4f00043036913a2bc401e14d26fdfb537d8d3f35
Dennis Schafroth <dennis@indexdata.com>
2014-03-26 16:38:59 +0100

    Add timeout, data format and from date setters

--- harvester/src/main/java/com/indexdata/masterkey/localindices/client/ClientTransport.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/client/ClientTransport.java
@@ -2,10 +2,13 @@ package com.indexdata.masterkey.localindices.client;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 
 public interface ClientTransport {
   
   void connect(URL url) throws IOException;
   RemoteFileIterator get(URL url) throws IOException, ClientTransportError;
-
+  void setTimeout(Integer seconds);
+  void setCompressedFormat(String mimetype);
+  void setFromDate(Date date);
 }
