19886cc1592d1053699e7a4bacabaa7cc39b8c98
Jakub Skoczen <jakub@indexdata.dk>
2014-05-19 16:07:05 +0200

    Use XMLUtils for parsing

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/XmlSplitter.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/XmlSplitter.java
@@ -4,40 +4,31 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 
 import org.xml.sax.ContentHandler;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
 
 import com.indexdata.masterkey.localindices.harvest.job.StorageJobLogger;
-import com.indexdata.xml.factory.XmlFactory;
+import com.indexdata.utils.XmlUtils;
 
 public class XmlSplitter  {
   private StorageJobLogger logger; 
   private ContentHandler handler;
-  SAXParserFactory spf;
 
   public XmlSplitter(RecordStorage storage, StorageJobLogger logger, ContentHandler handler) 
   	throws IOException, TransformerConfigurationException {
     this.logger = logger;
-    spf = XmlFactory.newSAXParserFactoryInstance();
     this.handler = handler;
   }
 
   public void processDataFromInputStream(InputStream input) throws TransformerException, ParserConfigurationException, SAXException 
-  {
-    SAXParser parser = spf.newSAXParser();
-    XMLReader reader = parser.getXMLReader();
-    reader.setContentHandler(handler);
-    
+  {    
     try {
       InputSource source = new InputSource(input);
-      reader.parse(source);
+      XmlUtils.read(source, handler);
     } catch (IOException ioe) {
       if (logger != null) 
 	logger.error("IOException in XML split", ioe);
