6b2b308cf6adb881e0d632d67fe01da5486a122b
Jakub Skoczen <jakub@indexdata.dk>
2014-09-04 00:20:01 +0200

    CSV-to-XML parser, test
    MKH-444

--- harvester/src/main/java/com/indexdata/masterkey/localindices/csv/CSVConverter.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/csv/CSVConverter.java
@@ -14,11 +14,14 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.text.ParseException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  *
@@ -51,13 +54,48 @@ public class CSVConverter {
   public void processViaDOM(InputStream is, MessageConsumer mc, boolean split) throws UnsupportedEncodingException, IOException {
     Reader reader = new InputStreamReader(is, charset);
     CSVParser parser = format.parse(reader);
-    Document doc = split ? XmlUtils.newDoc("items") : null;
+    Document doc = split ? null : XmlUtils.newDoc("rows");
+    Map<Integer, String> headers = null;
+    if (containsHeader) {
+      //invert the weird header map
+      Map<String, Integer> headerMap = parser.getHeaderMap();
+      headers = new HashMap<Integer, String>(headerMap.size());
+      for (Entry<String, Integer> entry : parser.getHeaderMap().entrySet()) {
+        headers.put(entry.getValue(), entry.getKey());
+      }
+    }
     for (CSVRecord r : parser) {
-      doc = split ? doc : XmlUtils.newDoc("item");
-      doc.createElement("field");
+      Element root;
+      if (split) {
+        //doc is null
+        doc = XmlUtils.newDoc("row");
+        root = doc.getDocumentElement();
+      } else {
+        root = doc.createElement("row");
+        doc.getDocumentElement().appendChild(root);
+      }
+      for (int i=0; i<r.size(); i++) {
+        Element fieldE = doc.createElement("field");
+        if (containsHeader) {
+          String name = headers.get(i);
+          //one indexed
+          name = name != null ? name : String.valueOf(i+1);
+          fieldE.setAttribute("name", name);
+        } else {
+          //one indexed
+          fieldE.setAttribute("name", String.valueOf(i+1));
+        }
+        fieldE.setTextContent(r.get(i));
+        root.appendChild(fieldE);
+      }
+      //sent chunk
+      if (split)
+        mc.accept(doc);
+    }
+    //sent whole doc
+    if (!split) {
       mc.accept(doc);
     }
-    
   }
   
 }
