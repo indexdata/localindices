c41e6d8012d82ee8594d1309776695ae157e8573
Niels Erik G. Nielsen <nielserik@indexdata.com>
2019-05-02 12:14:31 +0200

    Hijack 'new_zebrastorage' navigation label for inventory
     'edit_inventorystorage' navigation outcome works
     'new_zebrastorage' navigation come works
     but cannot make 'new_inventorystorage' navigation outcome work(?)
    
     Thus hijacking obsolete 'new_zebrastorage' label for now for inventory
     storage navigation.

--- harvester-admin/src/main/java/com/indexdata/masterkey/localindices/web/admin/controller/StorageController.java
+++ harvester-admin/src/main/java/com/indexdata/masterkey/localindices/web/admin/controller/StorageController.java
@@ -147,8 +147,10 @@ public class StorageController {
 
   public String prepareInventoryStorageToAdd() {
     logger.log(Level.INFO, "Invoked prepareInventoryStorageToAdd()");
+    logger.log(Level.INFO, "Instantiating InventoryStorageEntity");
     storage = new InventoryStorageEntity();
-    return "new_inventorystorage";
+    logger.log(Level.INFO, "Returning new_zebrastorage");
+    return "new_zebrastorage";
   }
 
   public String addStorage() {
