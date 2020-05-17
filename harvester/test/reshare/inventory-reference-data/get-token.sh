1a4ab5d7ae6949a370ae6346d0b3e7e96e6940f1
Niels Erik G. Nielsen <nielserik@indexdata.com>
2020-05-17 21:26:05 +0200

    Delete ReShare ref data scripts (moved to own project)

--- harvester/test/reshare/inventory-reference-data/get-token.sh
+++
@@ -1,9 +0,0 @@
-tenant=$1
-username=$2
-password=$3
-protocol=$4
-host=$5
-
-
-curl -s -X POST -D - -H "Content-type: application/json" -H "X-Okapi-Tenant: $tenant"  -d "{ \"username\": \"$username\", \"password\": \"$password\"}" $protocol://$host/authn/login | grep x-okapi-token | tr -d '\r' | cut -d " " -f2 
-
