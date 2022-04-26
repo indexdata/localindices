c8242916bf93f644cf1247d9eb7a8d8067a73de2
Niels Erik G. Nielsen <nielserik@indexdata.com>
2022-04-26 22:03:03 +0200

    Get source ID from JSON config (MODSI-17)
     - look for source ID (a UUID) in the harvestable's free-form JSON config, and use
      if present
     - else look for institutionId in the transformed record, and use if present
     - otherwise throw exception since source ID is mandatory in the shared index module

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/folio/ShareIndexUpdateContext.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/folio/ShareIndexUpdateContext.java
@@ -5,6 +5,9 @@ import com.indexdata.masterkey.localindices.entity.Storage;
 import com.indexdata.masterkey.localindices.harvest.job.StorageJobLogger;
 import com.indexdata.masterkey.localindices.harvest.storage.StorageException;
 import org.apache.log4j.Level;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.util.Map;
 
@@ -12,6 +15,7 @@ public class ShareIndexUpdateContext extends FolioUpdateContext {
 
   private static final String SHARED_INDEX_PATH = "sharedIndexPath";
   public String sharedIndexPath;
+  public String sourceId;
   public HourlyPerformanceStats timingsCreatingRecord;
   public HourlyPerformanceStats timingsTransformingRecord;
   public HourlyPerformanceStats timingsIndexEntry;
@@ -20,11 +24,34 @@ public class ShareIndexUpdateContext extends FolioUpdateContext {
     super(harvestable, logger);
     Storage storage = harvestable.getStorage();
     setStorageConfig(storage);
+    sourceId = getJsonStringProperty(getJsonConfig(), "sourceId");
     timingsIndexEntry = new HourlyPerformanceStats("Storing records", logger);
     timingsCreatingRecord = new HourlyPerformanceStats("Creating DOM for incoming record", logger);
     timingsCreatingRecord.setLogLevelForIntervals(Level.DEBUG);
     timingsTransformingRecord = new HourlyPerformanceStats("Transforming incoming record before storing", logger);
     timingsTransformingRecord.setLogLevelForIntervals(Level.DEBUG);
+  }
+
+  private JSONObject getJsonConfig () {
+    JSONObject json = new JSONObject();
+    try {
+      if (harvestable.getJson() == null || harvestable.getJson().isEmpty()) {
+        return json;
+      } else {
+        return (JSONObject) (new JSONParser()).parse(harvestable.getJson());
+      }
+    } catch (ParseException | ClassCastException pe) {
+      logger.error("Could not parse the harvestable's json config as a JSON object: " + pe.getMessage());
+      return json;
+    }
+  }
+
+  private static String getJsonStringProperty (JSONObject json, String name) {
+    if (json.containsKey(name)) {
+      return json.get(name).toString();
+    } else {
+      return null;
+    }
   }
 
   @Override
