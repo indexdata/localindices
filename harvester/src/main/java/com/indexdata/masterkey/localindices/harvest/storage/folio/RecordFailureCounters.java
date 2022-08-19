5978c2f43cad31e7a0ddc5b11826f6f04b863b59
Niels Erik G. Nielsen <nielserik@indexdata.com>
2022-08-19 17:27:21 +0200

    Do not assume request JSON is returned in error responses.
       Avoid null pointer: FOLIO Inventory Update is currently not always able to include the input JSON in error responses.

--- harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/folio/RecordFailureCounters.java
+++ harvester/src/main/java/com/indexdata/masterkey/localindices/harvest/storage/folio/RecordFailureCounters.java
@@ -1,21 +1,27 @@
 package com.indexdata.masterkey.localindices.harvest.storage.folio;
 
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class RecordFailureCounters {
-    protected final Map<String,Integer> errorsByShortErrorMessage = new HashMap<String,Integer>();
-    protected final Map<String,Integer> failedRecordsSavedByErrorMessage = new HashMap<String,Integer>();
+
+    protected final Map<String,Integer> errorsByShortErrorMessage = new HashMap<>();
+    protected final Map<String,Integer> failedRecordsSavedByErrorMessage = new HashMap<>();
     protected int failedRecordsSaved = 0;
 
      // TODO: maybe concatenate messages in case of multiple errors
     public void countFailedRecordsSaved (RecordWithErrors record) {
-        failedRecordsSaved++;
-        String message = record.errors.get(0).getMessageWithContext();
-        if (failedRecordsSavedByErrorMessage.containsKey(message)) {
-            failedRecordsSavedByErrorMessage.put(message,failedRecordsSavedByErrorMessage.get(message)+1);
-        } else {
-            failedRecordsSavedByErrorMessage.put(message, 1);
+        if (record.errors.size()>0) {
+            failedRecordsSaved++;
+            String message = record.errors.get(0).getMessageWithContext();
+            if (failedRecordsSavedByErrorMessage.containsKey(message)) {
+                failedRecordsSavedByErrorMessage.put(message,
+                        failedRecordsSavedByErrorMessage.get(message) + 1);
+            } else {
+                failedRecordsSavedByErrorMessage.put(message, 1);
+            }
+            record.logger.info("RecordFailureCounters was asked to count errors but error list had none.");
         }
     }
 
